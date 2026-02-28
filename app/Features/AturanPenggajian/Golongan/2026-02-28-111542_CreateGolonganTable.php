509e3da00bc3b6b5c810b2a263063803fce5c41f
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-02-28 14:59:28 +0700

    Add Golongan migration

---
+++ app/Features/AturanPenggajian/Golongan/2026-02-28-111542_CreateGolonganTable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Features\AturanPenggajian\Golongan;
+
+use App\Core\MigrationTemplate;
+use App\Core\DBType as T;
+
+class CreateBPJSTable extends MigrationTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'penggajian',
+            'golongan',
+            [
+                'no_golongan'   => T::ID8(),
+                'kode_golongan' => T::TEXT(),
+                'nama_golongan' => T::TEXT(),
+                'pendidikan'    => T::TEXT(),
+                'gaji_pokok'    => T::INT32(),
+            ],
+            ['no_golongan'],
+            [['nama_golongan'], ['kode_golongan']],
+
+        );
+    }
+}
