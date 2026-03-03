9401590ffdb7bdefd9ea525f01e31c6ced818783
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-03-03 13:46:43 +0700

    Add feature Email in Kontak

---
+++ app/Features/Kontak/Email/2026-03-06-093402_CreateEmailTable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Features\Kontak\Email;
+
+use App\Core\MigrationTemplate;
+use App\Core\DBType as T;
+
+class CreateEmailTable extends MigrationTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'kontak',
+            'email',
+            [
+                'id_email'     => T::ID32(),
+                'id_orang'     => T::ID32(),
+                'alamat_email' => T::TEXT(),
+            ],
+            ['id_email'],
+            [['alamat_email']],
+            [
+                [['id_orang'], 'person.orang', ['id_orang'], 'CASCADE', 'CASCADE'],
+            ],
+            [],
+        );
+    }
+}
