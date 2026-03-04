28819ccf306c5b0fbc4474aedb9b25daf4d35589
Aliyah <aliyahawwaliyah@gmail.com>
2026-03-04 20:25:20 +0700

    Fix feature Golongan Darah in Darah

---
+++ app/Features/Darah/GolonganDarah/2026-03-04-113402_CreateGolonganDarahTable.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Features\Darah\GolonganDarah;
+
+use App\Core\MigrationTemplate;
+use App\Core\DBType as T;
+
+class CreateGolonganDarahTable extends MigrationTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'darah',
+            'golongan_darah',
+            [
+                'id_golongan_darah'      => T::ID8(),
+                'nama_golongan_darah'    => T::TEXT(),
+            ],
+            ['id_golongan_darah'],
+            unique_key: [['nama_golongan_darah']],
+        );
+    }
+}
---
+++ app/Features/Person/Orang/2026-03-04-123402_CreateOrangTable.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Features\Person\Orang;
+
+use App\Core\MigrationTemplate;
+use App\Core\DBType as T;
+    
+/*
+* Tabel orang digunakan untuk menyimpan data identitas individu
+* yang terdaftar di dalam sistem.
+*
+* Data ini bersifat master dan dapat direlasikan dengan berbagai
+* fitur lain seperti donor, pasien, petugas, atau pengguna sistem.
+ */
+
+class CreateOrangTable extends MigrationTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'person',
+            'orang',
+            [
+                'id_orang'           => T::ID32(),
+                'nik'                => T::TEXT(),
+                'nama'               => T::TEXT(),
+                'id_jenis_kelamin'   => T::ID8(),
+                'id_agama'           => T::ID8(),
+                'id_pernikahan'      => T::ID8(),
+                'id_golongan_darah'  => T::ID8(),
+                'id_alamat'          => T::ID32(),
+                'tempat_lahir_prov'  => T::ID8(),
+                'tempat_lahir_kota'  => T::ID8(),
+                'tanggal_lahir'      => T::DATE(),
+            ],
+            ['id_orang'],
+            unique_key: [['nik']],
+            foreign_key: [
+                [['id_jenis_kelamin'], 'jenis_kelamin', ['id_jenis_kelamin'], 'CASCADE', 'CASCADE'],
+                [['id_agama'], 'agama', ['id_agama'], 'CASCADE', 'CASCADE'],
+                [['id_pernikahan'], 'pernikahan', ['id_pernikahan'], 'CASCADE', 'CASCADE'],
+                [['tempat_lahir_prov', 'tempat_lahir_kota'], 'lokasi.kota', ['id_provinsi', 'id_kota_lokal'], 'CASCADE', 'CASCADE'],
+                [['id_golongan_darah'], 'darah.golongan_darah', ['id_golongan_darah'], 'CASCADE', 'CASCADE'],
+                [['id_alamat'], 'lokasi.alamat', ['id_alamat'], 'CASCADE', 'CASCADE'],
+            ]
+        );
+    }
+}
