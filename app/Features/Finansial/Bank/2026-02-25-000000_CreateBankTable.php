b04b8938dc2bc8f5d5e3d398aa67d2591762506b
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-03-09 16:46:18 +0700

    Add feature Bank in Finansial

---
+++ app/Features/Finansial/Bank/2026-02-25-000000_CreateBankTable.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Features\Finansial\Bank;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateBankTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'finansial',
+            'bank',
+            [
+                'id'             => T::ID8(),
+                'kode'           => T::INT16()->nullable(),
+                'nama'           => T::TEXT()->nullable(),
+                'sebutan'        => T::TEXT()->nullable(),
+                'pemilik_id'     => T::ID8(),
+                'prinsip_id'     => T::ID8(),
+                'is_bank_devisa' => T::BOOL()->nullable(),
+                'mobile_app'     => T::TEXT(),
+                'link_playstore' => T::TEXT(),
+
+            ],
+            ['id'],
+            [['nama']],
+            [
+                [['pemilik_id'], 'pemilik_bank', ['id'], 'CASCADE', 'CASCADE'],
+                [['prinsip_id'], 'prinsip_bank', ['id'], 'CASCADE', 'CASCADE'],
+            ],
+            [],
+            true,
+            __DIR__ . '/bank.csv'
+        );
+    }
+}
