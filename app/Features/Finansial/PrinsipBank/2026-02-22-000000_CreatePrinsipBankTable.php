659377c3bb5ea64fdbf142af973ea181c2386744
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-03-09 16:45:55 +0700

    Add feature PrinsipBank in Finansial

---
+++ app/Features/Finansial/PrinsipBank/2026-02-22-000000_CreatePrinsipBankTable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Features\Finansial\PrinsipBank;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreatePrinsipBankTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'finansial',
+            'prinsip_bank',
+            [
+                'id'      => T::ID8(),
+                'prinsip' => T::TEXT(),
+            ],
+            ['id'],
+            [['prinsip']],
+            [],
+            [],
+            true,
+            __DIR__ . '/prinsip_bank.csv'
+        );
+    }
+}
