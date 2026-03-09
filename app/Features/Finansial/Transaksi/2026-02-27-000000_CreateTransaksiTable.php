8cbc65592547f0122b269553e0ad9a02adfbbd19
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-03-09 16:46:37 +0700

    Add feature Transaksi in Finansial

---
+++ app/Features/Finansial/Transaksi/2026-02-27-000000_CreateTransaksiTable.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Features\Finansial\Transaksi;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateTransaksiTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'finansial',
+            'transaksi',
+            [
+                'id_transaksi'      => T::ID8(),
+                'nama_transaksi'    => T::TEXT(),
+            ],
+            ['id_transaksi'],
+            [['nama_transaksi']],
+        );
+    }
+}
