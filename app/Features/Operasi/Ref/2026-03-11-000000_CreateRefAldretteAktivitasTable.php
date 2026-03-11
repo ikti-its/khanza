66f5822b3348a2c78cdc24a4fa92b758c5ccce8d
qoyyimil <qoyyimillj@gmail.com>
2026-03-11 16:38:22 +0700

    feat(operasi): add ref_aldrette_aktivitas migration and seed

---
+++ app/Features/Operasi/Ref/2026-03-11-000000_CreateRefAldretteAktivitasTable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Features\Operasi\Ref;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateRefAldretteAktivitasTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'operasi',
+        'ref_aldrette_aktivitas',
+        [
+            'id_aktivitas' => T::ID8(),
+            'nama_skala'   => T::TEXT(),
+            'nilai'        => T::INT8(),
+        ],
+        ['id_aktivitas'],
+        [],
+        [],
+        [],
+        true,
+        __DIR__ . '/aldrette_aktivitas.csv'
+    );
+}
+}
