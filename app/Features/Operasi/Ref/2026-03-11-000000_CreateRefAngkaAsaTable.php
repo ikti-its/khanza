2af04d3479ebf2e090d5d2e38865248ed6c3e67e
qoyyimil <qoyyimillj@gmail.com>
2026-03-11 16:45:18 +0700

    feat(operasi): add ref_angka_asa migration and seed

---
+++ app/Features/Operasi/Ref/2026-03-11-000000_CreateRefAngkaAsaTable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Features\Operasi\Ref;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateRefAngkaAsaTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'operasi',
+        'ref_angka_asa',
+        [
+            'id_asa'   => T::ID8(),
+            'nama_asa' => T::TEXT(),
+        ],
+        ['id_asa'],
+        [],
+        [],
+        [],
+        true,
+        __DIR__ . '/angka_asa.csv'
+    );
+}
+}
