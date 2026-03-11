5042b6ac640b78214d30f945d504f6b180014567
qoyyimil <qoyyimillj@gmail.com>
2026-03-11 22:51:13 +0700

    feat(operasi): add ref_posisi_pasien migration and seed

---
+++ app/Features/Operasi/Ref/2026-03-11-000000_CreateRefPosisiPasienTable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Features\Operasi\Ref;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateRefPosisiPasienTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'operasi',
+        'ref_posisi_pasien',
+        [
+            'id_posisi'   => T::ID8(),
+            'nama_posisi' => T::TEXT(),
+        ],
+        ['id_posisi'],
+        [],
+        [],
+        [],
+        true,
+        __DIR__ . '/posisi_pasien.csv'
+    );
+}
+}
