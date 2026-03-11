df764efa0c3684b892568c83787ebf47625237a6
qoyyimil <qoyyimillj@gmail.com>
2026-03-11 16:47:00 +0700

    feat(operasi): add ref_indikasi_pindah migration and seed

---
+++ app/Features/Operasi/Ref/2026-03-11-000000_CreateRefIndikasiPindahTable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Features\Operasi\Ref;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateRefIndikasiPindahTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'operasi',
+        'ref_indikasi_pindah',
+        [
+            'id_indikasi'   => T::ID8(),
+            'nama_indikasi' => T::TEXT(),
+        ],
+        ['id_indikasi'],
+        [],
+        [],
+        [],
+        true,
+        __DIR__ . '/indikasi_pindah.csv'
+    );
+}
+}
