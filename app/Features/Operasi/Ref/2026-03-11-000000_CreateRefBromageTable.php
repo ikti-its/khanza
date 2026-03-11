2b87f512bb1fbe66e8dc666ad375a23872890ce0
qoyyimil <qoyyimillj@gmail.com>
2026-03-11 16:45:40 +0700

    feat(operasi): add ref_bromage migration and seed

---
+++ app/Features/Operasi/Ref/2026-03-11-000000_CreateRefBromageTable.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Features\Operasi\Ref;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateRefAldretteAktivitasTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'operasi',
+        'ref_bromage',
+        [
+            'id_bromage'   => T::ID8(),
+            'nama_skala'   => T::TEXT(),
+            'tingkat_blok' => T::TEXT(),
+            'nilai'        => T::INT8(),
+            'gambar'       => T::TEXT(),
+        ],
+        ['id_bromage'],
+        [],
+        [],
+        [],
+        true,
+        __DIR__ . '/bromage.csv'
+    );
+}
+}
