73adf0f30a07e99db7cbc087f022f780e64f29d4
qoyyimil <qoyyimillj@gmail.com>
2026-03-11 22:52:11 +0700

    feat(operasi): add ref_status_operasi migration and seed

---
+++ app/Features/Operasi/Ref/2026-03-11-000000_CreateRefStatusOperasiTable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Features\Operasi\Ref;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateRefStatusOperasiTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'operasi',
+        'ref_status_operasi',
+        [
+            'id_status'   => T::ID8(),
+            'nama_status' => T::TEXT(),
+        ],
+        ['id_status'],
+        [],
+        [],
+        [],
+        true,
+        __DIR__ . '/status_operasi.csv'
+    );
+}
+}
