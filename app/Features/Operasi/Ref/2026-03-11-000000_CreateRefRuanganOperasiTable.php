f48524804fd1328c17a9f325b5eba6e9c856ad98
qoyyimil <qoyyimillj@gmail.com>
2026-03-11 22:51:59 +0700

    feat(operasi): add ref_ruangan_operasi migration and seed

---
+++ app/Features/Operasi/Ref/2026-03-11-000000_CreateRefRuanganOperasiTable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Features\Operasi\Ref;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateRefRuanganOperasiTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'operasi',
+        'ref_ruangan_operasi',
+        [
+            'id_ruangan'   => T::ID8(),
+            'kode_ruangan' => T::TEXT(),
+            'nama_ruangan' => T::TEXT(),
+        ],
+        ['id_ruangan'],
+        [['kode_ruangan']],
+        [],
+        [],
+        true,
+        __DIR__ . '/ruangan_operasi.csv'
+    );
+}
+}
