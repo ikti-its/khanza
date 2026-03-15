b5f0234420c189ccbd553a7ff215c9cefa2b75fe
qoyyimil <qoyyimillj@gmail.com>
2026-03-15 15:57:42 +0700

    feat(lab): add permintaan lab anatomi migration

---
+++ app/Features/Laboratorium/Anatomi/2026-03-15-000001_CreatePermintaanLabAnatomiTable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Features\Laboratorium\Anatomi;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreatePermintaanLabAnatomiTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'laboratorium',
+        'permintaan_lab_pa',
+        [
+            'id_permintaan_pa'          => T::ID32(),
+            'id_permintaan_lab'         => T::ID64(),
+            'tgl_pengambilan_bahan'     => T::DATETIME(),
+            'metode_diperoleh'          => T::TEXT(),
+            'lokasi_jaringan'           => T::TEXT(),
+            'bahan_pengawet'            => T::TEXT(),
+            'riwayat_lokasi_lab'        => T::TEXT(),
+            'riwayat_tgl_sebelumnya'    => T::DATE(),
+            'riwayat_no_pa_sebelumnya'  => T::TEXT(),
+            'riwayat_diagnosa_sebelumnya' => T::TEXT(),
+            'id_item_pemeriksaan'       => T::ID32(),
+        ],
+        ['id_permintaan_pa'],
+        [],
+        [
+            [['id_permintaan_lab'], 'laboratorium.permintaan_lab_header', ['id_permintaan'], 'CASCADE', 'CASCADE'],
+            [['id_item_pemeriksaan'], 'laboratorium.ref_item_pemeriksaan_lab', ['id_item_lab'], 'CASCADE', 'RESTRICT'],
+        ],
+        [['id_permintaan_lab']]
+    );
+}
+}
