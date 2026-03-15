ef4d17f635ae30944a5275bb3293592f5d7a1b95
qoyyimil <qoyyimillj@gmail.com>
2026-03-15 15:52:35 +0700

    feat(lab): add ref_kategori_lab migration and seed

---
+++ app/Features/Laboratorium/Ref/2026-03-15-000000_CreateRefKategoriLabTable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Features\Laboratorium\Ref;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateRefKategoriLabTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'laboratorium',
+        'ref_kategori_lab',
+        [
+            'id_kategori'   => T::ID8(),
+            'kode_kategori' => T::TEXT(),
+            'nama_kategori' => T::TEXT(),
+        ],
+        ['id_kategori'],
+        [['kode_kategori']],
+        [],
+        [],
+        true,
+        __DIR__ . '/kategori_lab.csv'
+    );
+}
+}
