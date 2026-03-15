9f8b4b04fa0a3c1d89b95180bfb4fa8d89c9d11e
qoyyimil <qoyyimillj@gmail.com>
2026-03-15 19:23:16 +0700

    feat(radiologi): add ref_item_rad migration and seed

---
+++ app/Features/Radiologi/Ref/2026-03-15-000000_CreateRefItemRadTable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Features\Radiolgi\Ref;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateRefItemRadTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'radiologi',
+        'ref_item_rad',
+        [
+            'id_item'          => T::ID32(),
+            'kode_periksa'     => T::TEXT(),
+            'nama_pemeriksaan' => T::TEXT(),
+            'tarif_dasar'      => T::F32(),
+        ],
+        ['id_item'],
+        [['kode_periksa']],
+        [],
+        [],
+        true,
+        __DIR__ . '/item_rad.csv'
+    );
+}
+}
