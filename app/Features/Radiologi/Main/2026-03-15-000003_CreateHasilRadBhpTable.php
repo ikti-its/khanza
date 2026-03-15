b6d861e06b9e814e6c56ad8621ba7551ba8cc739
qoyyimil <qoyyimillj@gmail.com>
2026-03-15 19:36:18 +0700

    feat(radiologi): add bhp hasil radiologi migration

---
+++ app/Features/Radiologi/Main/2026-03-15-000003_CreateHasilRadBhpTable.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Features\Radiolgi\Main;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateHasilRadBhpTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'radiologi',
+        'hasil_rad_bhp',
+        [
+            'id_rad_bhp'     => T::ID64(),
+            'id_hasil_rad'   => T::ID64(),
+            'id_barang_medis'=> T::UUID(),
+            'jumlah_pakai'   => T::F32(),
+            'satuan'         => T::TEXT(),
+            'harga_satuan'   => T::F32(),
+        ],
+        ['id_rad_bhp'],
+        [],
+        [
+            [['id_hasil_rad'], 'radiologi.hasil_rad', ['id_hasil_rad'], 'CASCADE', 'CASCADE'],
+            [['id_barang_medis'], 'sik.barang_medis_structure', ['id'], 'CASCADE', 'RESTRICT'],
+        ],
+        [['id_hasil_rad']]
+    );
+}
+}
