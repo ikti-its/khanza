264e3482ce50665031c70b92736e904cc289f2c9
qoyyimil <qoyyimillj@gmail.com>
2026-03-15 15:52:59 +0700

    feat(lab): add ref_status_permintaan migration and seed

---
+++ app/Features/Laboratorium/Ref/2026-03-15-000000_CreateRefStatusPermintaanTable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Features\Laboratorium\Ref;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateRefStatusPermintaanTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'laboratorium',
+        'ref_status_permintaan',
+        [
+            'id_status'   => T::ID8(),
+            'nama_status' => T::TEXT(),
+        ],
+        ['id_status'],
+        [],
+        [],
+        [],
+        true,
+        __DIR__ . '/status_permintaan.csv'
+    );
+}
+}
