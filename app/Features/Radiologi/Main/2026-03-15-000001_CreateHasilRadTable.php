e2c15eab4aac2949ea7655fd936e731854fbdc0c
qoyyimil <qoyyimillj@gmail.com>
2026-03-15 19:35:53 +0700

    feat(radiologi): add hasil radiologi migration

---
+++ app/Features/Radiologi/Main/2026-03-15-000001_CreateHasilRadTable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Features\Radiolgi\Main;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateHasilRadTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'radiologi',
+        'hasil_rad',
+        [
+            'id_hasil_rad'        => T::ID64(),
+            'id_permintaan_rad'   => T::ID64(),
+            'nomor_reg'           => T::TEXT(),
+            'kode_dokter_pj'      => T::TEXT(),
+            'id_petugas_rad'      => T::UUID(),
+            'kode_dokter_perujuk' => T::TEXT(),
+            'tgl_jam_hasil'       => T::DATETIME(),
+            'catatan'             => T::TEXT(),
+        ],
+        ['id_hasil_rad'],
+        [],
+        [
+            [['id_permintaan_rad'], 'radiologi.permintaan_rad', ['id_permintaan'], 'CASCADE', 'RESTRICT'],
+            [['nomor_reg'], 'sik.registrasi_structure', ['nomor_reg'], 'CASCADE', 'RESTRICT'],
+            [['kode_dokter_pj'], 'sik.dokter_structure', ['kode_dokter'], 'CASCADE', 'RESTRICT'],
+            [['id_petugas_rad'], 'sik.pegawai_structure', ['id'], 'CASCADE', 'RESTRICT'],
+            [['kode_dokter_perujuk'], 'sik.dokter_structure', ['kode_dokter'], 'CASCADE', 'RESTRICT'],
+        ],
+        [['nomor_reg'], ['tgl_jam_hasil']]
+    );
+}
+}
