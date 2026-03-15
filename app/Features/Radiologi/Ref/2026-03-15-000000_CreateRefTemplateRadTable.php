a373494c9edc30fc7faed48bf6090e989bce2f6a
qoyyimil <qoyyimillj@gmail.com>
2026-03-15 19:23:43 +0700

    feat(radiologi): add ref_template_rad migration and seed

---
+++ app/Features/Radiologi/Ref/2026-03-15-000000_CreateRefTemplateRadTable.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Features\Radiolgi\Ref;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateRefTemplateRadTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'radiologi',
+        'ref_template_rad',
+        [
+            'id_template'        => T::ID32(),
+            'nama_template'      => T::TEXT(),
+            'isi_teks_ekspertise'=> T::TEXT(),
+        ],
+        ['id_template'],
+        [],
+        [],
+        [],
+        true,
+        __DIR__ . '/template_rad.csv'
+    );
+}
+}
