1be10dd6fc318bfa9dd65bf5377160f074a18013
qoyyimil <qoyyimillj@gmail.com>
2026-03-15 19:36:07 +0700

    feat(radiologi): add tindakan hasil radiologi migration

---
+++ app/Features/Radiologi/Main/2026-03-15-000002_CreateHasilRadTindakanTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Features\Radiolgi\Main;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+    
+class CreateHasilRadTindakanTable extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'radiologi',
+        'hasil_rad_tindakan',
+        [
+            'id_hasil_tindakan'         => T::ID64(),
+            'id_hasil_rad'              => T::ID64(),
+            'id_item_rad'               => T::ID32(),
+            'tarif_tindakan'            => T::F32(),
+            'kilovoltage_kv'            => T::F32(),
+            'milliampere_second_mas'    => T::F32(),
+            'focus_film_distance_ffd'   => T::F32(),
+            'back_scatter_factor_bsf'   => T::F32(),
+            'inaktivasi'                => T::TEXT(),
+            'jumlah_penyinaran'         => T::INT32(),
+            'dosis_radiasi'             => T::TEXT(),
+            'hasil_ekspertise'          => T::TEXT(),
+            'id_template_rad'           => T::INT32()->nullable(),
+        ],
+        ['id_hasil_tindakan'],
+        [],
+        [
+            [['id_hasil_rad'], 'radiologi.hasil_rad', ['id_hasil_rad'], 'CASCADE', 'CASCADE'],
+            [['id_item_rad'], 'radiologi.ref_item_rad', ['id_item'], 'CASCADE', 'RESTRICT'],
+            [['id_template_rad'], 'radiologi.ref_template_rad', ['id_template'], 'CASCADE', 'RESTRICT'],
+        ],
+        [['id_hasil_rad']]
+    );
+}
+}
