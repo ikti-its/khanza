c1c14ec112ad23d7439b8ea4da195e85d929c1c2
Aliyah <aliyahawwaliyah@gmail.com>
2026-03-18 21:52:00 +0700

    Add feature Komponen Darah in Darah

---
+++ app/Features/Darah/KomponenDarah/2026-03-17-000000_CreateKomponenDarahTable.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Darah\KomponenDarah;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateKomponenDarahTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'darah',
+            'komponen_darah',
+            [
+                'id_komponen'           => T::ID8(),
+                'kode_komponen'         => T::TEXT(),
+                'nama_komponen'         => T::TEXT(),
+                'masa_berlaku_hari'     => T::INT16(),
+            ],
+            ['id_komponen'],
+            [['kode_komponen']],
+            [],
+            [],
+        );
+    }
+}
