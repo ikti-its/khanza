dbf8ce2f78a490e3af2606e84e42caa7c74fea67
Aliyah <aliyahawwaliyah@gmail.com>
2026-03-19 00:26:09 +0700

    Add feature Pendonor in Role

---
+++ app/Features/Role/Pendonor/2026-03-19-000000_CreatePendonorTable.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Role\Pendonor;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreatePendonorTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'role',
+            'pendonor',
+            [
+                'id_pendonor'                => T::ID32(),
+                'id_orang'                   => T::ID32(),
+                'nomor_kartu_pendonor'       => T::TEXT(),
+                'id_rhesus'                  => T::ID8(),
+                'id_status_pendonor'         => T::ID8(),
+            ],
+            ['id_pendonor'],
+            [['id_orang'], ['nomor_kartu_pendonor']],
+            [
+                [['id_orang'], 'person.orang', ['id_orang'], 'CASCADE', 'CASCADE'],
+                [['id_rhesus'], 'darah.rhesus', ['id_rhesus'], 'CASCADE', 'CASCADE'],
+                [['id_status_pendonor'], 'donor.status_pendonor', ['id_status_pendonor'], 'CASCADE', 'CASCADE'],
+            ],
+            [['id_status_pendonor']],
+        );
+    }
+}
