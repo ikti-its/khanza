b28649ce48f209c9c32c41d10bfd28b0a1a27afa
Aliyah <aliyahawwaliyah@gmail.com>
2026-03-19 14:18:29 +0700

    Add feature Kunjungan in Donor

---
+++ app/Features/Donor/Kunjungan/2026-03-21-000000_CreateKunjunganTable.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Donor\Kunjungan;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateKunjunganTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'donor',
+            'kunjungan',
+            [
+                'id_kunjungan'          => T::ID32(),
+                'nomor_kunjungan'       => T::TEXT(),
+                'id_pendonor'           => T::ID32(),
+                'tanggal_kunjungan'     => T::DATE(),
+                'nomor_antrian'         => T::INT16(),
+                'id_status_kunjungan'   => T::ID8(),
+            ],
+            ['id_kunjungan'],
+            [['nomor_kunjungan'], ['tanggal_kunjungan', 'nomor_antrian']],
+            [
+                [['id_pendonor'], 'role.pendonor', ['id_pendonor'], 'CASCADE', 'CASCADE'],
+                [['id_status_kunjungan'], 'status_kunjungan', ['id_status_kunjungan'], 'CASCADE', 'CASCADE'],
+            ],
+            [['id_pendonor']],
+        );
+    }
+}
