32b20894cf49381083e2ad57ae267d38094b3ef6
Aliyah <aliyahawwaliyah@gmail.com>
2026-03-24 22:21:59 +0700

    Add feature Shift Pengambilan Darah in Donor

---
+++ app/Features/Donor/ShiftPengambilanDarah/2026-03-29-000000_CreateShiftPengambilanDarahTable.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Donor\ShiftPengambilanDarah;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateShiftPengambilanDarahTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'donor',
+            'shift_pengambilan_darah',
+            [
+                'id_shift_pengambilan'      => T::ID8(),
+                'nama_shift'    => T::TEXT(),
+            ],
+            ['id_shift_pengambilan'],
+            [['nama_shift']],
+            [],
+            [],
+            true,
+            __DIR__ . '/shift_pengambilan_darah.csv'
+        );
+    }
+}
