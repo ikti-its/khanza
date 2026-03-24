7c1020d104467e415605b043135c9b9efefd4b7b
Aliyah <aliyahawwaliyah@gmail.com>
2026-03-24 21:29:01 +0700

    Add feature Lokasi Pengambilan Darah in Donor

---
+++ app/Features/Donor/LokasiPengambilanDarah/2026-03-28-000000_CreateLokasiPengambilanDarahTable.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Donor\LokasiPengambilanDarah;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateLokasiPengambilanDarahTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'donor',
+            'lokasi_pengambilan_darah',
+            [
+                'id_lokasi_pengambilan'    => T::ID8(),
+                'nama_lokasi_pengambilan'  => T::TEXT(),
+            ],
+            ['id_lokasi_pengambilan'],
+            [['nama_lokasi_pengambilan']],
+            [],
+            [],
+            true,
+            __DIR__ . '/lokasi_pengambilan_darah.csv'
+        );
+    }
+}
