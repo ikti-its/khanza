e991784bf52e171a641d6d843ee86915503b2f39
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-01 21:58:46 +0700

    Refactor: move feature Shift from Donor to Operasional

--- app/Features/Donor/PengambilanDarah/2026-03-30-000000_CreatePengambilanDarahTable.php
+++ app/Features/Donor/PengambilanDarah/2026-03-30-000000_CreatePengambilanDarahTable.php
@@ -16,7 +16,7 @@ class CreatePengambilanDarahTable extends DatabaseTemplate
                 'id_pengambilan_darah'    => T::ID32(),
                 'id_kunjungan'            => T::ID32(),
                 'tanggal_pengambilan'     => T::DATE(),
-                'id_shift_pengambilan'    => T::ID8(),
+                'id_shift'                => T::ID8(),
                 'id_jenis_donor'          => T::ID8(),
                 'id_lokasi_pengambilan'   => T::ID8(),
                 // 'id_petugas'              => T::ID32(),
@@ -25,7 +25,7 @@ class CreatePengambilanDarahTable extends DatabaseTemplate
             [['id_kunjungan']],
             [
                 [['id_kunjungan'], 'kunjungan', ['id_kunjungan'], 'CASCADE', 'CASCADE'],
-                [['id_shift_pengambilan'], 'shift_pengambilan_darah', ['id_shift_pengambilan'], 'CASCADE', 'CASCADE'],
+                [['id_shift'], 'operasional.shift', ['id_shift'], 'CASCADE', 'CASCADE'],
                 [['id_jenis_donor'], 'jenis_donor', ['id_jenis_donor'], 'CASCADE', 'CASCADE'],
                 [['id_lokasi_pengambilan'], 'lokasi_pengambilan_darah', ['id_lokasi_pengambilan'], 'CASCADE', 'CASCADE'],
                 // [['id_petugas'], 'role.petugas', ['id_petugas'], 'CASCADE', 'CASCADE'],
---
+++ app/Features/Operasional/Shift/2026-03-29-000000_CreateShiftTable.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasional\Shift;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateShiftTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'operasional',
+            'shift',
+            [
+                'id_shift'         => T::ID8(),
+                'nama_shift'       => T::TEXT(),
+            ],
+            ['id_shift'],
+            [['nama_shift']],
+            [],
+            [],
+            true,
+            __DIR__ . '/shift.csv'
+        );
+    }
+}
