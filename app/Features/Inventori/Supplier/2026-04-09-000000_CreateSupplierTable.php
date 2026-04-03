329b0c6a5480ea1e615c348b03fd3c79c2e876de
mas663 <affan8971@gmail.com>
2026-04-03 14:37:37 +0700

    feat(inventori): create supplier table

---
+++ app/Features/Inventori/Supplier/2026-04-09-000000_CreateSupplierTable.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Inventori\Supplier;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateSupplierTable extends DatabaseTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'inventori',
+            'supplier',
+            [
+                'id_supplier'   => T::ID32(),
+                'nama_supplier' => T::TEXT(),
+                'no_telp'       => T::TEXT()->nullable(),
+                'alamat'        => T::TEXT()->nullable(),
+            ],
+            ['id_supplier'],
+            [], // No Unique key since kode_supplier is omitted
+            [], // No Foreign key
+            [
+                ['nama_supplier']
+            ],
+            false,
+            ''
+        );
+    }
+}
