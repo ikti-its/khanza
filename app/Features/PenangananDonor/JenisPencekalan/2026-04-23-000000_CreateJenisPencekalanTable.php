a5199f0abcb13b6808253670b340b2ce63e9268c
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-03 21:15:43 +0700

    Add feature Jenis Pencekalan in Penanganan Donor

---
+++ app/Features/PenangananDonor/JenisPencekalan/2026-04-23-000000_CreateJenisPencekalanTable.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\JenisPencekalan;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateJenisPencekalanTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'penanganan_donor',
+            'jenis_pencekalan',
+            [
+                'id_jenis_pencekalan'      => T::ID8(),
+                'nama_jenis_pencekalan'    => T::TEXT(),
+            ],
+            ['id_jenis_pencekalan'],
+            [['nama_jenis_pencekalan']],
+            [],
+            [],
+            true,
+            __DIR__ . '/jenis_pencekalan.csv'
+        );
+    }
+}
