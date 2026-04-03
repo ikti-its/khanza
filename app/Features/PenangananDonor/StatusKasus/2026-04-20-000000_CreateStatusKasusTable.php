238c270edee447224efe161e38557251dd20a6d1
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-03 20:57:33 +0700

    Add feature StatusKasus in Penanganan Donor

---
+++ app/Features/PenangananDonor/StatusKasus/2026-04-20-000000_CreateStatusKasusTable.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\StatusKasus;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateStatusKasusTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'penanganan_donor',
+            'status_kasus',
+            [
+                'id_status_kasus'       => T::ID8(),
+                'nama_status_kasus'     => T::TEXT(),
+            ],
+            ['id_status_kasus'],
+            [['nama_status_kasus']],
+            [],
+            [],
+            true,
+            __DIR__ . '/status_kasus.csv'
+        );
+    }
+}
