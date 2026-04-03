0184c62e2cb5b91034c3f960c2aec8463985579c
mas663 <affan8971@gmail.com>
2026-04-03 15:01:22 +0700

    feat(inventori): create transaksi_stok table

---
+++ app/Features/Inventori/TransaksiStok/2026-04-13-000000_CreateTransaksiStokTable.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Inventori\TransaksiStok;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateTransaksiStokTable extends DatabaseTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'inventori',
+            'transaksi_stok',
+            [
+                'id_transaksi'   => T::ID32(),
+                'id_barang'      => T::ID32(),
+                
+                'tipe_transaksi' => T::TEXT(), 
+                
+                'qty'            => T::F64(),
+                
+                'tanggal'        => T::DATETIME(),
+            ],
+            ['id_transaksi'],
+            [],
+            [
+                [['id_barang'], 'barang', ['id_barang'], 'CASCADE', 'RESTRICT'],
+            ],
+            [
+                ['tanggal'],
+                ['tipe_transaksi']
+            ],
+            false,
+            ''
+        );
+    }
+}
