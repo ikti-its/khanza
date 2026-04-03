56e09ccb620021bd9555779cc6ff75bcc046cbb7
mas663 <affan8971@gmail.com>
2026-04-03 15:26:39 +0700

    feat(inventori): create permintaan_barang table

---
+++ app/Features/Inventori/PermintaanBarang/2026-04-15-000000_CreatePermintaanBarangTable.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Inventori\PermintaanBarang;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreatePermintaanBarangTable extends DatabaseTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'inventori',
+            'permintaan_barang',
+            [
+                'id_permintaan' => T::ID32(),
+                'tanggal'       => T::DATETIME(),
+                'status'        => T::TEXT(),
+            ],
+            ['id_permintaan'],
+            [],
+            [],
+            [],
+            false,
+            ''
+        );
+    }
+}
