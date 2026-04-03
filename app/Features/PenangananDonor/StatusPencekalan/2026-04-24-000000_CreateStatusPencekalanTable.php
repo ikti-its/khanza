3a69ffd5eb012a2a3d8a2cd8bba470f427c9d1fa
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-03 21:29:15 +0700

    Add feature Status Pencekalan in Penanganan Donor

---
+++ app/Features/PenangananDonor/StatusPencekalan/2026-04-24-000000_CreateStatusPencekalanTable.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\StatusPencekalan;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateStatusPencekalanTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'penanganan_donor',
+            'status_pencekalan',
+            [
+                'id_status_pencekalan'      => T::ID8(),
+                'nama_status_pencekalan'    => T::TEXT(),
+            ],
+            ['id_status_pencekalan'],
+            [['nama_status_pencekalan']],
+            [],
+            [],
+            true,
+            __DIR__ . '/status_pencekalan.csv'
+        );
+    }
+}
