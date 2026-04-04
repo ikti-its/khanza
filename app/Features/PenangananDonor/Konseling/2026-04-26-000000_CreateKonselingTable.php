f9cc4d40f271b473181ef96828e785f65f93db2a
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-04 15:44:15 +0700

    Add feature Konseling in Penanganan Donor

---
+++ app/Features/PenangananDonor/Konseling/2026-04-26-000000_CreateKonselingTable.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\Konseling;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateKonselingTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'penanganan_donor',
+            'konseling',
+            [
+                'id_konseling'          => T::ID32(),
+                'id_kasus'              => T::ID32(),
+                'tanggal_konseling'     => T::DATE(),
+                // 'id_petugas'            => T::ID32(),
+            ],
+            ['id_konseling'],
+            [['id_kasus']],
+            [
+                [['id_kasus'], 'kasus_reaktif', ['id_kasus'], 'CASCADE', 'CASCADE'],
+                // [['id_petugas'], 'role.petugas', ['id_petugas'], 'CASCADE', 'CASCADE'],
+            ],
+            [],
+        );
+    }
+}
