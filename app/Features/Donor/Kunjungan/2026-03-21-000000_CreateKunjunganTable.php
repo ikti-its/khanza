9a7c7a41933f9042b1c7c74158c0b3d2047da76e
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-05 22:25:06 +0700

    Fix column in feature Kunjungan

--- app/Features/Donor/Kunjungan/2026-03-21-000000_CreateKunjunganTable.php
+++ app/Features/Donor/Kunjungan/2026-03-21-000000_CreateKunjunganTable.php
@@ -14,14 +14,13 @@ class CreateKunjunganTable extends DatabaseTemplate
             'kunjungan',
             [
                 'id_kunjungan'          => T::ID32(),
-                'nomor_kunjungan'       => T::TEXT(),
                 'id_pendonor'           => T::ID32(),
                 'tanggal_kunjungan'     => T::DATE(),
                 'nomor_antrian'         => T::INT16(),
                 'id_status_kunjungan'   => T::ID8(),
             ],
             ['id_kunjungan'],
-            [['nomor_kunjungan'], ['tanggal_kunjungan', 'nomor_antrian']],
+            [['tanggal_kunjungan', 'nomor_antrian']],
             [
                 [['id_pendonor'], 'role.pendonor', ['id_pendonor'], 'CASCADE', 'CASCADE'],
                 [['id_status_kunjungan'], 'status_kunjungan', ['id_status_kunjungan'], 'CASCADE', 'CASCADE'],
