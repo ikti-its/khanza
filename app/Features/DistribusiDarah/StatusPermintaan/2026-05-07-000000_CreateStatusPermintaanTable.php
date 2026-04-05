df738429fde6b1318e6c8d101c41c33ccb7dbb51
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-06 01:27:25 +0700

    Add feature Status Permintaan in Distribusi Darah

---
+++ app/Features/DistribusiDarah/StatusPermintaan/2026-05-07-000000_CreateStatusPermintaanTable.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\DistribusiDarah\StatusPermintaan;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateStatusPermintaanTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'distribusi_darah',
+            'status_permintaan',
+            [
+                'id_status_permintaan'    => T::ID8(),
+                'nama_status_permintaan'  => T::TEXT(),
+            ],
+            ['id_status_permintaan'],
+            [['nama_status_permintaan']],
+            [],
+            [],
+            true,
+            __DIR__ . '/status_permintaan.csv'
+        );
+    }
+}
