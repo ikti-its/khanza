32e2e7eec5d90b9901bbd9ac7c7293042b05a1bd
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-05 12:02:53 +0700

    Add feature Rujukan in Penanganan Donor

---
+++ app/Features/PenangananDonor/Rujukan/2026-05-01-000000_CreateRujukanTable.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\Rujukan;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateRujukanTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'penanganan_donor',
+            'rujukan',
+            [
+                'id_rujukan'              => T::ID32(),
+                'id_kasus'                => T::ID32(),
+                'nomor_surat'             => T::TEXT(),
+                'tanggal_rujukan'         => T::DATE(),
+                'id_fasyankes'            => T::ID16(),
+            ],
+            ['id_rujukan'],
+            [['nomor_surat']],
+            [
+                [['id_kasus'], 'kasus_reaktif', ['id_kasus'], 'CASCADE', 'CASCADE'],
+                [['id_fasyankes'], 'fasyankes_rujukan', ['id_fasyankes'], 'CASCADE', 'CASCADE'],
+            ],
+            [['id_kasus']],
+        );
+    }
+}
