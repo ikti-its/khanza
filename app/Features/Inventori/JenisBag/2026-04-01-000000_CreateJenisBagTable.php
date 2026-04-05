bffb538832214b2aec48f537352b61287ca141e4
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-05 22:37:15 +0700

    Rename feature Jenis Kantong to Jenis Bag

---
+++ app/Features/Inventori/JenisBag/2026-04-01-000000_CreateJenisBagTable.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Inventori\JenisBag;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateJenisBagTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'inventori',
+            'jenis_bag',
+            [
+                'id_jenis_bag'     => T::ID8(),
+                'kode_jenis_bag'   => T::TEXT(),
+                'nama_jenis_bag'   => T::TEXT(),
+            ],
+            ['id_jenis_bag'],
+            [['kode_jenis_bag'], ['nama_jenis_bag']],
+            [],
+            [],
+            true,
+            __DIR__ . '/jenis_bag.csv'
+        );
+    }
+}
