60d63bab816afce06d9ccc47b857d2a7a216bd65
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-05 12:05:55 +0700

    Add feature Nilai Diagnostik in Uji Darah

---
+++ app/Features/UjiDarah/NilaiDiagnostik/2026-05-03-000000_CreateNilaiDiagnostikTable.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UjiDarah\NilaiDiagnostik;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateNilaiDiagnostikTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'uji_darah',
+            'nilai_diagnostik',
+            [
+                'id_nilai_diagnostik'      => T::ID8(),
+                'nama_nilai_diagnostik'    => T::TEXT(),
+            ],
+            ['id_nilai_diagnostik'],
+            [['nama_nilai_diagnostik']],
+            [],
+            [],
+            true,
+            __DIR__ . '/nilai_diagnostik.csv'
+        );
+    }
+}
