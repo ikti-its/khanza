88699519bfbfc077238ed2bd4f62a327f604c8d2
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-05 12:52:42 +0700

    Add feature Hasil Diagnostik in Uji Darah

---
+++ app/Features/UjiDarah/HasilDiagnostik/2026-05-04-000000_CreateHasilDiagnostikTable.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UjiDarah\HasilDiagnostik;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateHasilDiagnostikTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'uji_darah',
+            'hasil_diagnostik',
+            [
+                'id_diagnostik'          => T::ID32(),
+                'id_rujukan'             => T::ID32(),
+                'tanggal_hasil'          => T::DATE(),
+                'dokter_pemeriksa'       => T::TEXT(),
+            ],
+            ['id_diagnostik'],
+            [['id_rujukan']],
+            [
+                [['id_rujukan'], 'penanganan_donor.rujukan', ['id_rujukan'], 'CASCADE', 'CASCADE'],
+            ],
+            [],
+        );
+    }
+}
