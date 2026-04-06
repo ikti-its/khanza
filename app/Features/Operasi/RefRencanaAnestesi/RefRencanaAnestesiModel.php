403657702898ccf8fe63c358346836621dd7b60a
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:30:03 +0700

    Add RefRencanaAnestesiModel

---
+++ app/Features/Operasi/RefRencanaAnestesi/RefRencanaAnestesiModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefRencanaAnestesi;
+
+use App\Core\ModelTemplate;
+
+class RefRencanaAnestesiModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_rencana_anestesi',
+            'id_rencana_anestesi',
+            [
+                'id_rencana_anestesi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_rencana' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
