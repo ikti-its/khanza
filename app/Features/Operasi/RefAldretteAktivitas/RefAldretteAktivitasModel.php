cd41b10fddae8de53a782f928b5d084d88fae6d6
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:25:33 +0700

    Add RefAldretteAktivitasModel

---
+++ app/Features/Operasi/RefAldretteAktivitas/RefAldretteAktivitasModel.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefAldretteAktivitas;
+
+use App\Core\ModelTemplate;
+
+class RefAldretteAktivitasModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_aldrette_aktivitas',
+            'id_aktivitas',
+            [
+                'id_aktivitas' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_skala' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nilai' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
