41053e99fd208e3a31599023cdd4dcce9cc3a80a
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:27:08 +0700

    Add RefHubunganKeluargaModel

---
+++ app/Features/Operasi/RefHubunganKeluarga/RefHubunganKeluargaModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefHubunganKeluarga;
+
+use App\Core\ModelTemplate;
+
+class RefHubunganKeluargaModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_hubungan_keluarga',
+            'id_hubungan_keluarga',
+            [
+                'id_hubungan_keluarga' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_hubungan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
