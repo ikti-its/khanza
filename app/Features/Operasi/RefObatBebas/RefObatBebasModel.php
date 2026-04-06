e05b442b6d0fad184e33f03dc84dd5cd6e730b99
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:29:34 +0700

    Add RefObatBebasModel

---
+++ app/Features/Operasi/RefObatBebas/RefObatBebasModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefObatBebas;
+
+use App\Core\ModelTemplate;
+
+class RefObatBebasModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_obat_bebas',
+            'id_obat_bebas',
+            [
+                'id_obat_bebas' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_kategori' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
