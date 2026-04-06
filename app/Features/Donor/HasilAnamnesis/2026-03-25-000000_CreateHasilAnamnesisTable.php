52e70b4d951fda07c2fea899841adfc0c5ff832d
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-06 15:06:01 +0700

    Refactor: rename feature Kesimpulan Medis to Hasil Anamnesis and fix columns

---
+++ app/Features/Donor/HasilAnamnesis/2026-03-25-000000_CreateHasilAnamnesisTable.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Donor\HasilAnamnesis;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateHasilAnamnesisTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'donor',
+            'hasil_anamnesis',
+            [
+                'id_hasil'      => T::ID8(),
+                'nama_hasil'    => T::TEXT(),
+            ],
+            ['id_hasil'],
+            [['nama_hasil']],
+            [],
+            [],
+            true,
+            __DIR__ . '/hasil_anamnesis.csv'
+        );
+    }
+}
