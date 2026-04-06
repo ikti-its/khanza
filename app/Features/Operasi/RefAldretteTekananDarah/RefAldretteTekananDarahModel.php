655460e5862d20ef86f6e2defe088ec95eae7241
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:26:27 +0700

    Add RefAldretteTekananDarahModel

---
+++ app/Features/Operasi/RefAldretteTekananDarah/RefAldretteTekananDarahModel.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefAldretteTekananDarah;
+
+use App\Core\ModelTemplate;
+
+class RefAldretteTekananDarahModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_aldrette_tekanan_darah',
+            'id_td',
+            [
+                'id_td' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_skala' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nilai' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
