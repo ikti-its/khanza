c003c60de7bc9abb49b5c040a743773455d423e7
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:27:00 +0700

    Add RefBromageModel

---
+++ app/Features/Operasi/RefBromage/RefBromageModel.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefBromage;
+
+use App\Core\ModelTemplate;
+
+class RefBromageModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_bromage',
+            'id_bromage',
+            [
+                'id_bromage' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_skala' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'tingkat_blok' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nilai' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'gambar' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
