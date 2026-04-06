ebdfffa59634bc14c8397b47d84172c921b1296f
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-06 12:39:54 +0700

    Add GolonganDarahModel

---
+++ app/Features/Darah/GolonganDarah/GolonganDarahModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Darah\GolonganDarah;
+
+use App\Core\ModelTemplate;
+
+class GolonganDarahModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'darah',
+            'golongan_darah',
+            'id_golongan_darah',
+            [
+                'id_golongan_darah' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ], 
+                'nama_golongan_darah' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
