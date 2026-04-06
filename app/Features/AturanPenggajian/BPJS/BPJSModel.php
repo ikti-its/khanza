2c10285aa76194c93adcdf8739f843336a96d189
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-04-06 11:35:19 +0700

    Add BPJSModel

---
+++ app/Features/AturanPenggajian/BPJS/BPJSModel.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\AturanPenggajian\BPJS;
+
+use App\Core\ModelTemplate;
+
+class Model extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'penggajian',
+            'bpjs',
+            'no_bpjs',
+            [
+                'no_bpjs' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ], 
+                'nama_program' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ], 
+                'penyelenggara' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'tarif' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'batas_atas' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'batas_bawah' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
