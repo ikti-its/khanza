6fe5f2aaf1f7b50efa7d840a22f1fe8133334f39
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:27:50 +0700

    Add RefKeadaanUmumTransferModel

---
+++ app/Features/Operasi/RefKeadaanUmumTransfer/RefKeadaanUmumTransferModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefKeadaanUmumTransfer;
+
+use App\Core\ModelTemplate;
+
+class RefKeadaanUmumTransferModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_keadaan_umum_transfer',
+            'id_keadaan_umum',
+            [
+                'id_keadaan_umum' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_keadaan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
