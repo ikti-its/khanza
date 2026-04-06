890c7bb1fe6a3a1df857f8ac28785afcf1281b34
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-06 14:13:48 +0700

    Add KunjunganModel

---
+++ app/Features/Donor/Kunjungan/KunjunganModel.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Donor\Kunjungan;
+
+use App\Core\ModelTemplate;
+
+class KunjunganModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'donor',
+            'kunjungan',
+            'id_kunjungan',
+            [
+                'id_kunjungan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_pendonor' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'tanggal_kunjungan' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nomor_antrian' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_status_kunjungan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
