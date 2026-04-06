b5b39192cf9bf39c3502d3ddf16446b7058f419e
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:29:57 +0700

    Add RefPremedikasiModel

---
+++ app/Features/Operasi/RefPremedikasi/RefPremedikasiModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefPremedikasi;
+
+use App\Core\ModelTemplate;
+
+class RefPremedikasiModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_premedikasi',
+            'id_premedikasi',
+            [
+                'id_premedikasi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_premedikasi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
