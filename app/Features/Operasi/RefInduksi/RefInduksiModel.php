38b4d80675d85e02d6b8b32c8bbf9e407e3cb789
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:27:25 +0700

    Add RefInduksiModel

---
+++ app/Features/Operasi/RefInduksi/RefInduksiModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefInduksi;
+
+use App\Core\ModelTemplate;
+
+class RefInduksiModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_induksi',
+            'id_induksi',
+            [
+                'id_induksi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_induksi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
