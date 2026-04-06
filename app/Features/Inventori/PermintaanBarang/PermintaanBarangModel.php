b2a11b44227ea654981eeb4d2f50e71fcc34e325
mas663 <affan8971@gmail.com>
2026-04-06 13:27:17 +0700

    feat(inventori): create PermintaanBarang model

---
+++ app/Features/Inventori/PermintaanBarang/PermintaanBarangModel.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Inventori\PermintaanBarang;
+
+use App\Core\ModelTemplate;
+
+class PermintaanBarangModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'inventori',
+            'permintaan_barang',
+            'id_permintaan',
+            [
+                'id_permintaan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ], 
+                'tanggal' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ], 
+                'status' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
