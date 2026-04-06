1f62f8437380be1bf4d95ccd13aa95d6a33b74fa
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:29:50 +0700

    Add RefPosisiPasienModel

---
+++ app/Features/Operasi/RefPosisiPasien/RefPosisiPasienModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefPosisiPasien;
+
+use App\Core\ModelTemplate;
+
+class RefPosisiPasienModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_posisi_pasien',
+            'id_posisi',
+            [
+                'id_posisi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_posisi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
