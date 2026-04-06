509bd8857f6512b3f1e6d2a60501f9dc587cbeda
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:29:26 +0700

    Add RefMetodeTransferModel

---
+++ app/Features/Operasi/RefMetodeTransfer/RefMetodeTransferModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefMetodeTransfer;
+
+use App\Core\ModelTemplate;
+
+class RefMetodeTransferModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_metode_transfer',
+            'id_metode',
+            [
+                'id_metode' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_metode' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
