15bfac3dd2f06f959ee957a916d7e8bcd031526b
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-06 14:14:53 +0700

    Add StatusKunjunganModel

---
+++ app/Features/Donor/StatusKunjungan/StatusKunjunganModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Donor\StatusKunjungan;
+
+use App\Core\ModelTemplate;
+
+class StatusKunjunganModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'donor',
+            'status_kunjungan',
+            'id_status_kunjungan',
+            [
+                'id_status_kunjungan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_status_kunjungan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
