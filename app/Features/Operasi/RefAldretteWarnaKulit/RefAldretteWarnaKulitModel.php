77872c3beb2633a6119488c01c22b2ced595022c
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:26:41 +0700

    Add RefAldretteWarnaKulitModel

---
+++ app/Features/Operasi/RefAldretteWarnaKulit/RefAldretteWarnaKulitModel.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefAldretteWarnaKulit;
+
+use App\Core\ModelTemplate;
+
+class RefAldretteWarnaKulitModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_aldrette_warna_kulit',
+            'id_warna',
+            [
+                'id_warna' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_skala' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nilai' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
