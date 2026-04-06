d397c27c026b770b3f153ca6c4f81a7dc0506849
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:25:54 +0700

    Add RefAldretteKesadaranModel

---
+++ app/Features/Operasi/RefAldretteKesadaran/RefAldretteKesadaranModel.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefAldretteKesadaran;
+
+use App\Core\ModelTemplate;
+
+class RefAldretteKesadaranModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_aldrette_kesadaran',
+            'id_kesadaran',
+            [
+                'id_kesadaran' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_skala' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nilai' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
