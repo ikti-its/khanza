7d767400be6c7e3ddcc968e6ab97ff4601d2d04b
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-06 15:23:31 +0700

    Add LokasiPengambilanDarahModel

---
+++ app/Features/Donor/LokasiPengambilanDarah/LokasiPengambilanDarahModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Donor\LokasiPengambilanDarah;
+
+use App\Core\ModelTemplate;
+
+class LokasiPengambilanDarahModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'donor',
+            'lokasi_pengambilan_darah',
+            'id_lokasi_pengambilan',
+            [
+                'id_lokasi_pengambilan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_lokasi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
