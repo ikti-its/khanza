528bee42af0c7bdd294bdb370638417e6072589c
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-06 15:13:19 +0700

    Refactor: rename feature Pemeriksaan Fisik to Skrining Donor and fix columns

---
+++ app/Features/Donor/SkriningDonor/2026-03-26-000000_CreateSkriningDonorTable.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Donor\SkriningDonor;
+
+use App\Core\DatabaseTemplate;
+use App\Core\DatabaseType as T;
+
+class CreateSkriningDonorTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'donor',
+            'skrining_donor',
+            [
+                'id_skrining'          => T::ID32(),
+                'id_kunjungan'         => T::ID32(),
+                'sistolik'             => T::INT16(),
+                'diastolik'            => T::INT16(),
+                'berat_badan'          => T::F32(),
+                'kadar_hemoglobin'     => T::F32(),
+                'suhu_tubuh'           => T::F32(),
+                'id_hasil_anamnesis'   => T::ID8(),
+            ],
+            ['id_skrining'],
+            [['id_kunjungan']],
+            [
+                [['id_kunjungan'], 'kunjungan', ['id_kunjungan'], 'CASCADE', 'CASCADE'],
+                [['id_hasil_anamnesis'], 'hasil_anamnesis', ['id_hasil'], 'CASCADE', 'CASCADE'],
+            ],
+            [],
+        );
+    }
+}
