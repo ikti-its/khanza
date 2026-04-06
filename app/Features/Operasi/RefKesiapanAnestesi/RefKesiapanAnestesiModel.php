4d596b5dbcfcd8add173a52c92362f012a3f3091
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:29:11 +0700

    Add RefKesiapanAnestesiModel

---
+++ app/Features/Operasi/RefKesiapanAnestesi/RefKesiapanAnestesiModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefKesiapanAnestesi;
+
+use App\Core\ModelTemplate;
+
+class RefKesiapanAnestesiModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_kesiapan_anestesi',
+            'id_kesiapan',
+            [
+                'id_kesiapan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_kesiapan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
