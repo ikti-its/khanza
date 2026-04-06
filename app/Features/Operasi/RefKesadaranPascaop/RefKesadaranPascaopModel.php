5f66558a54fd1b33599d6d7cae29282f0d081bbf
qoyyimil <qoyyimillj@gmail.com>
2026-04-06 13:29:01 +0700

    Add RefKesadaranPascaopModel

---
+++ app/Features/Operasi/RefKesadaranPascaop/RefKesadaranPascaopModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefKesadaranPascaop;
+
+use App\Core\ModelTemplate;
+
+class RefKesadaranPascaopModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_kesadaran_pascaop',
+            'id_kesadaran',
+            [
+                'id_kesadaran' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_kesadaran' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
