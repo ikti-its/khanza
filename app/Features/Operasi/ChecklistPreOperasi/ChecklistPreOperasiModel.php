9e3e577bdb13fa0d7eebb3177b97bdceb9c71f9f
qoyyimil <qoyyimillj@gmail.com>
2026-04-07 13:32:17 +0700

    Add ChecklistPreOperasiModel

---
+++ app/Features/Operasi/ChecklistPreOperasi/ChecklistPreOperasiModel.php
@@ -0,0 +1,111 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\ChecklistPreOperasi;
+
+use App\Core\ModelTemplate;
+
+class ChecklistPreOperasiModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'BASE',
+            'operasi',
+            'checklist_pre_operasi',
+            'id_checklist',
+            [
+                'id_checklist' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'nomor_reg' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'waktu_checklist' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'sn_cn' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'kode_dokter_bedah' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'kode_dokter_anestesi' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'tindakan' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'is_identitas_sesuai' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_keadaan_umum' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_penandaan_area' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'is_ijin_bedah' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'is_ijin_anestesi' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_ijin_transfusi' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_persiapan_darah' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'ket_persiapan_darah' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_perlengkapan_khusus' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_petugas_ruangan' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_petugas_ok' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+            ],
+        );
+    }
+}
