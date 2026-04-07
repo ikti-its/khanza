f5fd9ba886fbfa9ca1671f70ba810308801d4269
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-07 11:20:04 +0700

    Add feature Permintaan Darah Detail in Distribusi Darah

---
+++ app/Features/DistribusiDarah/PermintaanDarahDetail/2026-05-09-000000_CreatePermintaanDarahDetailTable.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\DistribusiDarah\PermintaanDarahDetail;
+
+use App\Core\Database\Template;
+use App\Core\Database\Type as T;
+
+class CreatePermintaanDarahDetailTable extends Template
+{
+    public function __construct(){
+        parent::__construct(
+            'distribusi_darah',
+            'permintaan_darah_detail',
+            [
+                'id_permintaan_detail'      => T::ID32(),
+                'id_permintaan'             => T::INT32(),
+                'id_komponen'               => T::INT8(),
+                'id_golongan_darah'         => T::INT8(),
+                'id_rhesus'                 => T::INT8(),
+                'jumlah'                    => T::INT8(),
+            ],
+            ['id_permintaan_detail'],
+            [['id_permintaan', 'id_komponen', 'id_golongan_darah', 'id_rhesus']],
+            [
+                [['id_permintaan'], 'permintaan_darah', ['id_permintaan'], 'CASCADE', 'CASCADE'],
+                [['id_komponen'], 'darah.komponen_darah', ['id_komponen'], 'CASCADE', 'CASCADE'],
+                [['id_golongan_darah'], 'darah.golongan_darah', ['id_golongan_darah'], 'CASCADE', 'CASCADE'],
+                [['id_rhesus'], 'darah.rhesus', ['id_rhesus'], 'CASCADE', 'CASCADE'],
+            ],
+            [],
+        );
+    }
+}
