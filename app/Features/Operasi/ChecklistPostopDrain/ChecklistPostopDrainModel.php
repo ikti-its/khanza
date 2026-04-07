e198f39be4a9d52a452f92dd418d111d9ca5b8c6
qoyyimil <qoyyimillj@gmail.com>
2026-04-07 13:31:57 +0700

    Add ChecklistPostopDrainModel

---
+++ app/Features/Operasi/ChecklistPostopDrain/ChecklistPostopDrainModel.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\ChecklistPostopDrain;
+
+use App\Core\ModelTemplate;
+
+class ChecklistPostopDrainModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'BASE',
+            'operasi',
+            'checklist_postop_drain',
+            'id_drain',
+            [
+                'id_drain' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_checklist_post' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_ketersediaan' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'jumlah' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'letak' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'warna' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+            ],
+        );
+    }
+}
