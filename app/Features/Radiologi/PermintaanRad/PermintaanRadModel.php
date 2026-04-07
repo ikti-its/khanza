16e0e0b866401eb52467d002eb109db52d4becba
qoyyimil <qoyyimillj@gmail.com>
2026-04-07 20:24:16 +0700

    Add PermintaanRadModel

---
+++ app/Features/Radiologi/PermintaanRad/PermintaanRadModel.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Radiologi\PermintaanRad;
+
+use App\Core\ModelTemplate;
+
+class PermintaanRadModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'BASE',
+            'radiologi',
+            'permintaan_rad',
+            'id_permintaan',
+            [
+                'id_permintaan' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'no_permintaan' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'nomor_reg' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'kode_dokter_perujuk' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'tgl_jam_permintaan' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'informasi_tambahan' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'indikasi_klinis' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_status_permintaan' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_item_rad' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+            ],
+        );
+    }
+}
