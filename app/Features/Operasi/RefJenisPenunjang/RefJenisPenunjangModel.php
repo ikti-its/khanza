0cd4d089f26109394414f50ccc9244f76983e0d2
qoyyimil <qoyyimillj@gmail.com>
2026-04-07 12:55:31 +0700

    Add RefJenisPenunjangModel

---
+++ app/Features/Operasi/RefJenisPenunjang/RefJenisPenunjangModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefJenisPenunjang;
+
+use App\Core\ModelTemplate;
+
+class RefJenisPenunjangModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'operasi',
+            'ref_jenis_penunjang',
+            'id_jenis_penunjang',
+            [
+                'id_jenis_penunjang' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_jenis' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+            ],
+        );
+    }
+}
