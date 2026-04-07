647b0e3cafb685086e2b651814bc883fb9c5301a
qoyyimil <qoyyimillj@gmail.com>
2026-04-07 18:26:46 +0700

    Add RefStatusPermintaanModel

---
+++ app/Features/Laboratorium/RefStatusPermintaan/RefStatusPermintaanModel.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Laboratorium\RefStatusPermintaan;
+
+use App\Core\ModelTemplate;
+
+class RefStatusPermintaanModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'REFS',
+            'laboratorium',
+            'ref_status_permintaan',
+            'id_status',
+            [
+                'id_status' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'nama_status' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+            ],
+        );
+    }
+}
