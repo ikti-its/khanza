e6e43c550462288dc313fb5c4c30f47dab4d5905
qoyyimil <qoyyimillj@gmail.com>
2026-04-07 20:25:07 +0700

    Add RefSkriningKesadaranModel

---
+++ app/Features/RawatJalan/SkriningRJ/RefSkriningKesadaran/RefSkriningKesadaranModel.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\RawatJalan\SkriningRJ\RefSkriningKesadaran;
+
+use App\Core\ModelTemplate;
+
+class RefSkriningKesadaranModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'REFS',
+            'skrining_rj',
+            'ref_skrining_kesadaran',
+            'id_kesadaran',
+            [
+                'id_kesadaran' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'kesadaran' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+            ],
+        );
+    }
+}
