70d6ffe4d94b11b7715d4cef07918ba756ab763d
qoyyimil <qoyyimillj@gmail.com>
2026-04-07 13:33:58 +0700

    Add PenyerahanPasienPeralatanModel

---
+++ app/Features/Operasi/PenyerahanPasienPeralatan/PenyerahanPasienPeralatanModel.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\PenyerahanPasienPeralatan;
+
+use App\Core\ModelTemplate;
+
+class PenyerahanPasienPeralatanModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'BASE',
+            'operasi',
+            'penyerahan_pasien_peralatan',
+            'id',
+            [
+                'id' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_penyerahan' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_peralatan' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'keterangan' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+            ],
+        );
+    }
+}
