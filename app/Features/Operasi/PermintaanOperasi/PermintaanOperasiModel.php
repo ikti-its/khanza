05313b2f9f2a8dc0ab96ddbe30e3c47ea1a197a7
qoyyimil <qoyyimillj@gmail.com>
2026-04-07 13:34:06 +0700

    Add PermintaanOperasiModel

---
+++ app/Features/Operasi/PermintaanOperasi/PermintaanOperasiModel.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\PermintaanOperasi;
+
+use App\Core\ModelTemplate;
+
+class PermintaanOperasiModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'BASE',
+            'operasi',
+            'permintaan_operasi',
+            'id_permintaan',
+            [
+                'id_permintaan' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'nomor_reg' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'kode_dokter' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'tanggal_minta' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'is_cito' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+            ],
+        );
+    }
+}
