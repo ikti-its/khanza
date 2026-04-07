b11be1db21a28c91d3e91ccc1ef2a1e63670f966
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 00:01:40 +0700

    Add StatusStokModel

---
+++ app/Features/InventoriDarah/StatusStok/StatusStokModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\InventoriDarah\StatusStok;
+
+use App\Core\ModelTemplate;
+
+class StatusStokModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'inventori_darah',
+            'status_stok',
+            'id_status_stok',
+            [
+                'id_status_stok' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_status_stok' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
