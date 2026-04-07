ed76fb60098d5625cf50aff022333b910d676bd8
qoyyimil <qoyyimillj@gmail.com>
2026-04-07 13:33:43 +0700

    Add PengkajianPreopModel

---
+++ app/Features/Operasi/PengkajianPreop/PengkajianPreopModel.php
@@ -0,0 +1,76 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\PengkajianPreop;
+
+use App\Core\ModelTemplate;
+
+class PengkajianPreopModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'BASE',
+            'operasi',
+            'pengkajian_preop',
+            'id_pengkajian_pre',
+            [
+                'id_pengkajian_pre' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'nomor_reg' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'kode_dokter_bedah' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'waktu_pengkajian' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'ringkasan_klinik' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'pemeriksaan_fisik' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'pemeriksaan_diagnostik' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'diagnosa_pre_operasi' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'rencana_tindakan' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'persiapan_khusus' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'terapi_pre_operasi' => [
+                    'allowed' => true,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+            ],
+        );
+    }
+}
