23b79ec801c36617651d0553e164231c2389c5f0
qoyyimil <qoyyimillj@gmail.com>
2026-04-07 18:24:54 +0700

    Add RefItemPemeriksaanLabModel

---
+++ app/Features/Laboratorium/RefItemPemeriksaanLab/RefItemPemeriksaanLabModel.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Laboratorium\RefItemPemeriksaanLab;
+
+use App\Core\ModelTemplate;
+
+class RefItemPemeriksaanLabModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'REFS',
+            'laboratorium',
+            'ref_item_pemeriksaan_lab',
+            'id_item_lab',
+            [
+                'id_item_lab' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'id_kategori' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'kode_periksa' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'nama_item' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'tarif' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+            ],
+        );
+    }
+}
