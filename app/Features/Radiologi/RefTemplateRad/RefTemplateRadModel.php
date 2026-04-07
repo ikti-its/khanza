e72835088602f58e47b5a835b2f507365651cebc
qoyyimil <qoyyimillj@gmail.com>
2026-04-07 20:24:38 +0700

    Add RefTemplateRadModel

---
+++ app/Features/Radiologi/RefTemplateRad/RefTemplateRadModel.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Radiologi\RefTemplateRad;
+
+use App\Core\ModelTemplate;
+
+class RefTemplateRadModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'REFS',
+            'radiologi',
+            'ref_template_rad',
+            'id_template',
+            [
+                'id_template' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'nama_template' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'isi_teks_ekspertise' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+            ],
+        );
+    }
+}
