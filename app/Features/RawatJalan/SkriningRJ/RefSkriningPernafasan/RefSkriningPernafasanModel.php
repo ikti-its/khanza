61e2e3adc36ccd8af2c08d09a9f413ff57056127
qoyyimil <qoyyimillj@gmail.com>
2026-04-07 20:25:26 +0700

    Add RefSkriningPernafasanModel

---
+++ app/Features/RawatJalan/SkriningRJ/RefSkriningPernafasan/RefSkriningPernafasanModel.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\RawatJalan\SkriningRJ\RefSkriningPernafasan;
+
+use App\Core\ModelTemplate;
+
+class RefSkriningPernafasanModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'REFS',
+            'skrining_rj',
+            'ref_skrining_pernafasan',
+            'id_pernafasan',
+            [
+                'id_pernafasan' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+                'pernafasan' => [
+                    'allowed' => false,
+                    'rules' => '',
+                    'errors' => [],
+                ],
+            ],
+        );
+    }
+}
