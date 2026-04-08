4cbb48b47c3e96a2f09f0a9c59402a3941bdde99
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 11:42:39 +0700

    Add PulauModel

---
+++ app/Features/Lokasi/Pulau/PulauModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Lokasi\Pulau;
+
+use App\Core\ModelTemplate;
+
+class PulauModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'lokasi',
+            'pulau',
+            'id_pulau',
+            [
+                'id_pulau' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_pulau' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
