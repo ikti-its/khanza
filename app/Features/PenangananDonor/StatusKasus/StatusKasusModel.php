b84b81992206495af96dc1c821aeb08779ea2ca7
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 04:09:13 +0700

    Add StatusKasusModel

---
+++ app/Features/PenangananDonor/StatusKasus/StatusKasusModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\StatusKasus;
+
+use App\Core\ModelTemplate;
+
+class StatusKasusModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'penanganan_donor',
+            'status_kasus',
+            'id_status_kasus',
+            [
+                'id_status_kasus' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_status_kasus' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
