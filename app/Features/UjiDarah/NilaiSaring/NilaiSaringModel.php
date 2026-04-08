f9296652b5b98bbadd21653971795f630a9969b8
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 11:15:34 +0700

    Add NilaiSaringModel

---
+++ app/Features/UjiDarah/NilaiSaring/NilaiSaringModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UjiDarah\NilaiSaring;
+
+use App\Core\ModelTemplate;
+
+class NilaiSaringModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'uji_darah',
+            'nilai_saring',
+            'id_nilai_saring',
+            [
+                'id_nilai_saring' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_nilai_saring' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
