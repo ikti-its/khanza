ef1ce605a13e7c7646d022ce8efab43f65ccad29
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 03:32:37 +0700

    Add FasyankesRujukanModel

---
+++ app/Features/PenangananDonor/FasyankesRujukan/FasyankesRujukanModel.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\FasyankesRujukan;
+
+use App\Core\ModelTemplate;
+
+class FasyankesRujukanModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'penanganan_donor',
+            'fasyankes_rujukan',
+            'id_fasyankes',
+            [
+                'id_fasyankes' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_fasyankes' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_alamat' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'kode_pos' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
