3127fd897f706b28f00cc2904d0f32eaed9361bd
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 04:10:36 +0700

    Add StatusPencekalanModel

---
+++ app/Features/PenangananDonor/StatusPencekalan/StatusPencekalanModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\StatusPencekalan;
+
+use App\Core\ModelTemplate;
+
+class StatusPencekalanModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'penanganan_donor',
+            'status_pencekalan',
+            'id_status_pencekalan',
+            [
+                'id_status_pencekalan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_status_pencekalan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
