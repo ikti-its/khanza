31fc2427a510d779d7028aa94f08baa184bcc7bb
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 03:47:38 +0700

    Add KasusReaktifDetailModel

---
+++ app/Features/PenangananDonor/KasusReaktifDetail/KasusReaktifDetailModel.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\KasusReaktifDetail;
+
+use App\Core\ModelTemplate;
+
+class KasusReaktifDetailModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'penanganan_donor',
+            'kasus_reaktif_detail',
+            'id_kasus_detail',
+            [
+                'id_kasus_detail' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_kasus' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_uji_saring_detail' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
