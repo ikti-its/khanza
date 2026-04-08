312f5b339e4695f7667e0829ec3baad76a06638e
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 10:48:30 +0700

    Add PendonorModel

---
+++ app/Features/Role/Pendonor/PendonorModel.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Role\Pendonor;
+
+use App\Core\ModelTemplate;
+
+class PendonorModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'role',
+            'pendonor',
+            'id_pendonor',
+            [
+                'id_pendonor' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_orang' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nomor_pendonor' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_rhesus' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_status_pendonor' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
