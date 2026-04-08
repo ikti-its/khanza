1357d4188580ddbdf4f3fbfd4152e15ff97d2063
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 11:06:53 +0700

    Add HasilDiagnostikModel

---
+++ app/Features/UjiDarah/HasilDiagnostik/HasilDiagnostikModel.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UjiDarah\HasilDiagnostik;
+
+use App\Core\ModelTemplate;
+
+class HasilDiagnostikModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'uji_darah',
+            'hasil_diagnostik',
+            'id_diagnostik',
+            [
+                'id_diagnostik' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_rujukan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'tanggal_hasil' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'dokter_pemeriksa' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
