bb4523f0e85618aeb89ce692a2265d3eb5cbec0b
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 04:08:05 +0700

    Add RujukanModel

---
+++ app/Features/PenangananDonor/Rujukan/RujukanModel.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\Rujukan;
+
+use App\Core\ModelTemplate;
+
+class RujukanModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'penanganan_donor',
+            'rujukan',
+            'id_rujukan',
+            [
+                'id_rujukan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_kasus' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nomor_surat' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'tanggal_rujukan' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_fasyankes' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
