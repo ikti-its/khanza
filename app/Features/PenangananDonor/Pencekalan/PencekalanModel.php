16c78fc3ca351fae8d51cc186bac0c3e73203e51
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 04:01:39 +0700

    Add PencekalanModel

---
+++ app/Features/PenangananDonor/Pencekalan/PencekalanModel.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\Pencekalan;
+
+use App\Core\ModelTemplate;
+
+class PencekalanModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'penanganan_donor',
+            'pencekalan',
+            'id_pencekalan',
+            [
+                'id_pencekalan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_kunjungan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_jenis_pencekalan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'tanggal_mulai' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'tanggal_selesai' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_shift' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_petugas' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'keterangan' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_status_pencekalan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
