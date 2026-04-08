67d9ddc58a73a1cc0230755d90131133a7d781a9
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 10:15:49 +0700

    Add PemusnahanDarahModel

---
+++ app/Features/Pemusnahan/PemusnahanDarah/PemusnahanDarahModel.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Pemusnahan\PemusnahanDarah;
+
+use App\Core\ModelTemplate;
+
+class PemusnahanDarahModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'pemusnahan',
+            'pemusnahan_darah',
+            'id_pemusnahan',
+            [
+                'id_pemusnahan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'tanggal_pemusnahan' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_petugas' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'keterangan' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
