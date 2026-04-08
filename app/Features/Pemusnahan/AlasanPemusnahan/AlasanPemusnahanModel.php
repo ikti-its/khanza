ef2c94b2d43d05b93cfaa6bacc4cece8cce199d9
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 10:15:20 +0700

    Add AlasanPemusnahanModel

---
+++ app/Features/Pemusnahan/AlasanPemusnahan/AlasanPemusnahanModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Pemusnahan\AlasanPemusnahan;
+
+use App\Core\ModelTemplate;
+
+class AlasanPemusnahanModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'pemusnahan',
+            'alasan_pemusnahan',
+            'id_alasan',
+            [
+                'id_alasan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_alasan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
