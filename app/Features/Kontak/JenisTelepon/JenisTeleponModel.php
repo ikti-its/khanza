577e189be622c28cd02f50d6c07990053bb3abe8
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 12:09:47 +0700

    Add JenisTeleponModel

---
+++ app/Features/Kontak/JenisTelepon/JenisTeleponModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Kontak\JenisTelepon;
+
+use App\Core\ModelTemplate;
+
+class JenisTeleponModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'kontak',
+            'jenis_telepon',
+            'id_jenis_telepon',
+            [
+                'id_jenis_telepon' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_jenis_telepon' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
