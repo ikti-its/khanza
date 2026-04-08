0638727c7063aa059ea0ffdc7a6865068c5fa588
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 11:56:28 +0700

    Add AlamatModel

---
+++ app/Features/Lokasi/Alamat/AlamatModel.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Lokasi\Alamat;
+
+use App\Core\ModelTemplate;
+
+class AlamatModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'lokasi',
+            'alamat',
+            'id_alamat',
+            [
+                'id_alamat' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_provinsi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_kota_lokal' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_kecamatan_lokal' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_desa_lokal' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'rw' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'rt' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'alamat_lengkap' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
