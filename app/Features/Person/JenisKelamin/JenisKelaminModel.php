ff7cc7ed2c58377f69d91d9d99162196b8a99854
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 11:26:22 +0700

    Add JenisKelaminModel

---
+++ app/Features/Person/JenisKelamin/JenisKelaminModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Person\JenisKelamin;
+
+use App\Core\ModelTemplate;
+
+class JenisKelaminModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'person',
+            'jenis_kelamin',
+            'id_jenis_kelamin',
+            [
+                'id_jenis_kelamin' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_jenis_kelamin' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
