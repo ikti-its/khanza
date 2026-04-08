c1d5cd83b53da7ee2f10b961c57b19af6e2d6f35
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 12:16:45 +0700

    Add TeleponModel

---
+++ app/Features/Kontak/Telepon/TeleponModel.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Kontak\Telepon;
+
+use App\Core\ModelTemplate;
+
+class TeleponModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'kontak',
+            'telepon',
+            'id_telepon',
+            [
+                'id_telepon' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_orang' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nomor_telepon' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'jenis_telepon' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_provider' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
