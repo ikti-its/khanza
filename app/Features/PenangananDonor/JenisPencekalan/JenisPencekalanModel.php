5efe477429b21cacb3c477e76dcc2044fc0bea17
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 03:36:50 +0700

    Add JenisPencekalanModel

---
+++ app/Features/PenangananDonor/JenisPencekalan/JenisPencekalanModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\JenisPencekalan;
+
+use App\Core\ModelTemplate;
+
+class JenisPencekalanModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'penanganan_donor',
+            'jenis_pencekalan',
+            'id_jenis_pencekalan',
+            [
+                'id_jenis_pencekalan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_jenis_pencekalan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
