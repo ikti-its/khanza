5ed4df39ba6aa7cb380ca0bff75cdf6bdb27b3b2
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 04:03:17 +0700

    Add PertanyaanKonselingModel

---
+++ app/Features/PenangananDonor/PertanyaanKonseling/PertanyaanKonselingModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\PertanyaanKonseling;
+
+use App\Core\ModelTemplate;
+
+class PertanyaanKonselingModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'penanganan_donor',
+            'pertanyaan_konseling',
+            'id_pertanyaan',
+            [
+                'id_pertanyaan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'teks_pertanyaan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
