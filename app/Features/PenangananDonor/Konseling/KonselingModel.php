cbcacf659469e4baa71af924d8ecafccfa31b425
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 03:49:46 +0700

    Add KonselingModel

---
+++ app/Features/PenangananDonor/Konseling/KonselingModel.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\PenangananDonor\Konseling;
+
+use App\Core\ModelTemplate;
+
+class KonselingModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'penanganan_donor',
+            'konseling',
+            'id_konseling',
+            [
+                'id_konseling' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_kasus' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'tanggal_konseling' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_petugas' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
