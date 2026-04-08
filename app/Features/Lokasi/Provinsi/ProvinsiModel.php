9138d0099f0d1912a9a47786383601616abcbd53
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 11:46:37 +0700

    Add ProvinsiModel

---
+++ app/Features/Lokasi/Provinsi/ProvinsiModel.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Lokasi\Provinsi;
+
+use App\Core\ModelTemplate;
+
+class ProvinsiModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'lokasi',
+            'provinsi',
+            'id_provinsi',
+            [
+                'id_pulau' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_provinsi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_provinsi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
