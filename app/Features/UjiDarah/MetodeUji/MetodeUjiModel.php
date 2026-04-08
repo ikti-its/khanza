5a6846062e7beaeac004724d848a3b437fe27830
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 11:13:31 +0700

    Add MetodeUjiModel

---
+++ app/Features/UjiDarah/MetodeUji/MetodeUjiModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UjiDarah\MetodeUji;
+
+use App\Core\ModelTemplate;
+
+class MetodeUjiModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'uji_darah',
+            'metode_uji',
+            'id_metode_uji',
+            [
+                'id_metode_uji' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_metode' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
