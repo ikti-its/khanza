5ae591b5b8d71063ac6fd5cc34ea30a3bbb4a040
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 11:12:37 +0700

    Add HasilUjiSaringDetailModel

---
+++ app/Features/UjiDarah/HasilUjiSaringDetail/HasilUjiSaringDetailModel.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UjiDarah\HasilUjiSaringDetail;
+
+use App\Core\ModelTemplate;
+
+class HasilUjiSaringDetailModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'uji_darah',
+            'hasil_uji_saring_detail',
+            'id_uji_saring_detail',
+            [
+                'id_uji_saring_detail' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_uji_saring' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_parameter_uji' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_nilai_saring' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nilai_absorbance' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
