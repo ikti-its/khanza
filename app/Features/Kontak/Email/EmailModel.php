68554beeab0552f1de674e9fff6876805ac9e540
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 12:06:28 +0700

    Add EmailModel

---
+++ app/Features/Kontak/Email/EmailModel.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Kontak\Email;
+
+use App\Core\ModelTemplate;
+
+class EmailModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'kontak',
+            'email',
+            'id_email',
+            [
+                'id_email' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_orang' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'alamat_email' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
