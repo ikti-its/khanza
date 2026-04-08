badef4cd353c131505601c52bc89ae501558246e
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 10:17:36 +0700

    Add PemusnahanDarahDetailModel

---
+++ app/Features/Pemusnahan/PemusnahanDarahDetail/PemusnahanDarahDetailModel.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Pemusnahan\PemusnahanDarahDetail;
+
+use App\Core\ModelTemplate;
+
+class PemusnahanDarahDetailModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'pemusnahan',
+            'pemusnahan_darah_detail',
+            'id_pemusnahan_detail',
+            [
+                'id_pemusnahan_detail' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_pemusnahan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_stok_darah' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_alasan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
