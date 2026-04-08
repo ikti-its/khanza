41c266f20e286d6ab7278d068022886e4e07cbd6
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-08 11:14:18 +0700

    Add NilaiDiagnostikModel

---
+++ app/Features/UjiDarah/NilaiDiagnostik/NilaiDiagnostikModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UjiDarah\NilaiDiagnostik;
+
+use App\Core\ModelTemplate;
+
+class NilaiDiagnostikModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'uji_darah',
+            'nilai_diagnostik',
+            'id_nilai_diagnostik',
+            [
+                'id_nilai_diagnostik' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_nilai_diagnostik' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
