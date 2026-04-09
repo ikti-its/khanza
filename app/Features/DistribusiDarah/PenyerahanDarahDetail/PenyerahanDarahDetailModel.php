d4629cf1bbd8c36fad7d840f1dcf40f1b43ec9e2
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 16:48:38 +0700

    Add PenyerahanDarahDetailModel

---
+++ app/Features/DistribusiDarah/PenyerahanDarahDetail/PenyerahanDarahDetailModel.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\DistribusiDarah\PenyerahanDarahDetail;
+
+use App\Core\ModelTemplate;
+
+class PenyerahanDarahDetailModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'distribusi_darah',
+            'penyerahan_darah_detail',
+            'id_penyerahan_detail',
+            [
+                'id_penyerahan_detail' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_penyerahan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_stok_darah' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'jasa_sarana' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'paket_bhp' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'kso' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'manajemen' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'total' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
