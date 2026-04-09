4ec04c57d1dcc0a9c1107b25eb144975cb5c3e7a
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 15:57:21 +0700

    Add PermintaanDarahModel

---
+++ app/Features/DistribusiDarah/PermintaanDarah/PermintaanDarahModel.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\DistribusiDarah\PermintaanDarah;
+
+use App\Core\ModelTemplate;
+
+class PermintaanDarahModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'distribusi_darah',
+            'permintaan_darah',
+            'id_permintaan',
+            [
+                'id_permintaan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'no_rawat' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'tanggal_permintaan' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'kode_dokter_pengirim' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_status_permintaan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
