d602b3c264ced0e581395c648c2e97f8944d3761
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 16:19:19 +0700

    Add StatusPermintaanModel

---
+++ app/Features/DistribusiDarah/StatusPermintaan/StatusPermintaanModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\DistribusiDarah\StatusPermintaan;
+
+use App\Core\ModelTemplate;
+
+class StatusPermintaanModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'distribusi_darah',
+            'status_permintaan',
+            'id_status_permintaan',
+            [
+                'id_status_permintaan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_status_permintaan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
