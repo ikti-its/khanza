8545ebd938e91a8cff629d6e9faf01f56d61097c
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-09 16:20:42 +0700

    Add StatusPembayaranModel

---
+++ app/Features/DistribusiDarah/StatusPembayaran/StatusPembayaranModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\DistribusiDarah\StatusPembayaran;
+
+use App\Core\ModelTemplate;
+
+class StatusPembayaranModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'distribusi_darah',
+            'status_pembayaran',
+            'id_status_pembayaran',
+            [
+                'id_status_pembayaran' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_status_pembayaran' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
