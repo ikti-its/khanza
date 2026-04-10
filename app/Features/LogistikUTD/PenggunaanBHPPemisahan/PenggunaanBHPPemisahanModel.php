9660b443a4da29c0736324fa43b6a35f70014e5c
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-10 23:29:03 +0700

    Add PenggunaanBHPPemisahanModel

---
+++ app/Features/LogistikUTD/PenggunaanBHPPemisahan/PenggunaanBHPPemisahanModel.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\LogistikUTD\PenggunaanBHPPemisahan;
+
+use App\Core\ModelTemplate;
+
+class PenggunaanBHPPemisahanModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'logistik_utd',
+            'penggunaan_bhp_pemisahan',
+            'id_bhp_pemisahan',
+            [
+                'id_bhp_pemisahan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_pemisahan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_jenis_bhp' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_barang_medis' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_barang_penunjang' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'jumlah' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'harga' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
