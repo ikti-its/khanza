359b3130e6cf2d5f02a8935b3ff92ec2a6e70851
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-10 15:01:07 +0700

    Add feature Pengambilan Medis in Logistik UTD

---
+++ app/Features/LogistikUTD/PengambilanMedis/2026-05-15-000000_CreatePengambilanMedisTable.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\LogistikUTD\PengambilanMedis;
+
+use App\Core\Database\Template;
+use App\Core\Database\Type as T;
+
+class CreatePengambilanMedisTable extends Template
+{
+    public function __construct(){
+        parent::__construct(
+            'logistik_utd',
+            'pengambilan_medis',
+            [
+                'id_pengambilan_medis'      => T::ID32(),
+                'id_barang'                 => T::UUID(),
+                'jumlah'                    => T::INT32(),
+                'harga_beli'                => T::F64(),
+                'nama_bangsal'              => T::TEXT(),
+                'tanggal_pengambilan'       => T::DATETIME(),
+                'keterangan'                => T::TEXT(),
+                'nomor_batch'               => T::TEXT()->nullable(),
+                'nomor_faktur'              => T::TEXT()->nullable(),
+            ],
+            ['id_pengambilan_medis'],
+            [],
+            [
+                // [['id_barang'], 'sik.barang_medis_structure', ['id'], 'CASCADE', 'CASCADE'],
+            ],
+            [['id_barang'], ['tanggal_pengambilan']],
+        );
+    }
+}
