5af2f7de5c57e9f629e0b75a5b50182743f437a6
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-10 23:12:15 +0700

    Add feature Penggunaan BHP Pemisahan in Logistik UTD

---
+++ app/Features/LogistikUTD/PenggunaanBHPPemisahan/2026-05-20-000000_CreatePenggunaanBHPPemisahanTable.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\LogistikUTD\PenggunaanBHPPemisahan;
+
+use App\Core\Database\Template;
+use App\Core\Database\Type as T;
+
+class CreatePenggunaanBHPPemisahanTable extends Template
+{
+    public function __construct(){
+        parent::__construct(
+            'logistik_utd',
+            'penggunaan_bhp_pemisahan',
+            [
+                'id_bhp_pemisahan'          => T::ID32(),
+                'id_pemisahan'              => T::INT32(),
+                'id_jenis_bhp'              => T::INT8(),
+                'id_barang_medis'           => T::UUID()->nullable(),
+                'id_barang_penunjang'       => T::INT32()->nullable(),
+                'jumlah'                    => T::INT32(),
+                'harga'                     => T::F64(),
+            ],
+            ['id_bhp_pemisahan'],
+            [],
+            [
+                [['id_pemisahan'], 'inventori_darah.pemisahan_komponen', ['id_pemisahan'], 'CASCADE', 'CASCADE'],
+                [['id_jenis_bhp'], 'jenis_bhp', ['id_jenis_bhp'], 'CASCADE', 'CASCADE'],
+                // [['id_barang_medis'], 'sik.barang_medis_structure',['id'], 'CASCADE', 'CASCADE'],
+                // [['id_barang_penunjang'], 'inventori_non_medis.barang',['id_barang'], 'CASCADE', 'CASCADE'],
+            ],
+            [['id_pemisahan'], ['id_jenis_bhp']],
+        );
+    }
+}
