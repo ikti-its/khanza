6540bf9b548cbca85b9aa0d742ddd6e2eca6e74f
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-10 22:27:21 +0700

    Add feature Penggunaan BHP Donor in Logistik UTD

---
+++ app/Features/LogistikUTD/PenggunaanBHPDonor/2026-05-19-000000_CreatePenggunaanBHPDonorTable.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\LogistikUTD\PenggunaanBHPDonor;
+
+use App\Core\Database\Template;
+use App\Core\Database\Type as T;
+
+class CreatePenggunaanBHPDonorTable extends Template
+{
+    public function __construct(){
+        parent::__construct(
+            'logistik_utd',
+            'penggunaan_bhp_donor',
+            [
+                'id_bhp_donor'              => T::ID32(),
+                'id_pengambilan_darah'      => T::INT32(),
+                'id_jenis_bhp'              => T::INT8(),
+                'id_barang_medis'           => T::UUID()->nullable(),
+                'id_barang_penunjang'       => T::INT32()->nullable(),
+                'jumlah'                    => T::INT32(),
+                'harga'                     => T::F64(),
+            ],
+            ['id_bhp_donor'],
+            [],
+            [
+                [['id_pengambilan_darah'], 'donor.pengambilan_darah', ['id_pengambilan_darah'], 'CASCADE', 'CASCADE'],
+                [['id_jenis_bhp'], 'jenis_bhp', ['id_jenis_bhp'], 'CASCADE', 'CASCADE'],
+                // [['id_barang_medis'], 'sik.barang_medis_structure',['id'], 'CASCADE', 'CASCADE'],
+                // [['id_barang_penunjang'], 'inventori_non_medis.barang',['id_barang'], 'CASCADE', 'CASCADE'],
+            ],
+            [['id_pengambilan_darah'], ['id_jenis_bhp']],
+        );
+    }
+}
