b69c8704a6edebf6156a6c6cb19c72c8c3437b11
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-13 05:16:45 +0700

    Add KebutuhanKhususModel

---
+++ app/Features/TriaseUGD/KebutuhanKhusus/KebutuhanKhususModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\TriaseUGD\KebutuhanKhusus;
+
+use App\Core\ModelTemplate;
+
+class KebutuhanKhususModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'triase_ugd',
+            'kebutuhan_khusus',
+            'id_kebutuhan',
+            [
+                'id_kebutuhan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_kebutuhan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
