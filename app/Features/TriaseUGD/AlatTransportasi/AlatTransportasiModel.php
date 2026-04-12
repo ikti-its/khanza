0c325b0e9f3e3045981a3ccaaa8deb08ca6aea82
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-13 03:57:55 +0700

    Add AlatTransportasiModel

---
+++ app/Features/TriaseUGD/AlatTransportasi/AlatTransportasiModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\TriaseUGD\AlatTransportasi;
+
+use App\Core\ModelTemplate;
+
+class AlatTransportasiModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'triase_ugd',
+            'alat_transportasi',
+            'id_transportasi',
+            [
+                'id_transportasi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_transportasi' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
