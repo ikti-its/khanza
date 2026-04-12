45cc19b82b368e568aa897dd1b82d0364709018d
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-13 04:48:18 +0700

    Add TriasePemeriksaanModel

---
+++ app/Features/TriaseUGD/TriasePemeriksaan/TriasePemeriksaanModel.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\TriaseUGD\TriasePemeriksaan;
+
+use App\Core\ModelTemplate;
+
+class TriasePemeriksaanModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'triase_ugd',
+            'triase_pemeriksaan',
+            'id_pemeriksaan',
+            [
+                'id_pemeriksaan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'kode_pemeriksaan' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_pemeriksaan' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
