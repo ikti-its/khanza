2598dcbec302ecbfcdb4cabcd786d8b5777d5ee4
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-13 06:03:23 +0700

    Add DataTriasePrimerModel

---
+++ app/Features/TriaseUGD/DataTriasePrimer/DataTriasePrimerModel.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\TriaseUGD\DataTriasePrimer;
+
+use App\Core\ModelTemplate;
+
+class DataTriasePrimerModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'triase_ugd',
+            'data_triase_primer',
+            'id_triase_primer',
+            [
+                'id_triase_primer' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_triase' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'keluhan_utama' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_kebutuhan_khusus' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'catatan' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_plan_primer' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'tanggal_triase' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'id_petugas' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
