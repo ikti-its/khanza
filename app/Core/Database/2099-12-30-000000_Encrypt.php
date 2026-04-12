4a73dc65cddbf7c6ecb7bd73d01c5f76672b208f
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-04-12 14:15:23 +0700

    Temporarily disable encryption & audit for performance

--- app/Core/Database/2099-12-30-000000_Encrypt.php
+++ app/Core/Database/2099-12-30-000000_Encrypt.php
@@ -9,16 +9,25 @@ class Encrypt extends Migration
     const string PATH = APPPATH . 'Core/Database/Encrypt/';
     public function up()
     {
-        $this->db->query('CREATE EXTENSION IF NOT EXISTS pgcrypto;');
-        echo "halo";
-        $this->db->query(file_get_contents(self::PATH . 'rename_original_table.sql'));
-        $this->db->query(file_get_contents(self::PATH . 'create_encrypted_table.sql'));
-        $this->db->query(file_get_contents(self::PATH . 'create_encrypted_view.sql'));
+        // $config = new \Config\Database()->default;
+        // $config['database'] = env('database.default.khanza_db');
+        // $this->db = \Config\Database::connect($config);
+        // $this->forge = \Config\Database::forge($this->db);
+
+        // $this->db->query('CREATE EXTENSION IF NOT EXISTS pgcrypto;');
+        // $this->db->query(file_get_contents(self::PATH . 'rename_original_table.sql'));
+        // $this->db->query(file_get_contents(self::PATH . 'create_encrypted_table.sql'));
+        // $this->db->query(file_get_contents(self::PATH . 'create_encrypted_view.sql'));
     }
     public function down()
     {
-        $this->db->query(file_get_contents(self::PATH . 'drop_function.sql'));
-        $this->db->query(file_get_contents(self::PATH . 'drop_encrypted_view.sql'));
-        $this->db->query(file_get_contents(self::PATH . 'drop_encrypted_table.sql'));
+        // $config = new \Config\Database()->default;
+        // $config['database'] = env('database.default.khanza_db');
+        // $this->db = \Config\Database::connect($config);
+        // $this->forge = \Config\Database::forge($this->db);
+
+        // $this->db->query(file_get_contents(self::PATH . 'drop_function.sql'));
+        // $this->db->query(file_get_contents(self::PATH . 'drop_encrypted_view.sql'));
+        // $this->db->query(file_get_contents(self::PATH . 'drop_encrypted_table.sql'));
     }
 }
--- app/Core/Database/2099-12-31-000000_Audit.php
+++ app/Core/Database/2099-12-31-000000_Audit.php
@@ -11,12 +11,23 @@ class Audit extends Migration
     const string PATH = APPPATH . 'Core/Database/Audit/';
     public function up()
     {
-        $this->db->query(file_get_contents(self::PATH . 'create_audit_table.sql'));
-        $this->db->query(file_get_contents(self::PATH . 'create_audit_view.sql'));
+        // $config = new \Config\Database()->default;
+        // $config['database'] = env('database.default.khanza_db');
+        // $this->db = \Config\Database::connect($config);
+        // $this->forge = \Config\Database::forge($this->db);
+
+        // $this->db->query('CREATE EXTENSION IF NOT EXISTS pgcrypto;');
+        // $this->db->query(file_get_contents(self::PATH . 'create_audit_table.sql'));
+        // $this->db->query(file_get_contents(self::PATH . 'create_audit_view.sql'));
     }
     public function down()
     {
-        $this->db->query(file_get_contents(self::PATH . 'drop_audit_table.sql'));
-        $this->db->query(file_get_contents(self::PATH . 'drop_audit_view.sql'));
+        // $config = new \Config\Database()->default;
+        // $config['database'] = env('database.default.khanza_db');
+        // $this->db = \Config\Database::connect($config);
+        // $this->forge = \Config\Database::forge($this->db);
+
+        // $this->db->query(file_get_contents(self::PATH . 'drop_audit_table.sql'));
+        // $this->db->query(file_get_contents(self::PATH . 'drop_audit_view.sql'));
     }
 }
