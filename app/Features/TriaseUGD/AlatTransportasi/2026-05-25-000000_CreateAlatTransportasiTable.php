9c5464d65f66d35bd796127123078030373d9f93
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-13 03:54:53 +0700

    Add feature Alat Transportasi in Triase UGD

---
+++ app/Features/TriaseUGD/AlatTransportasi/2026-05-25-000000_CreateAlatTransportasiTable.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\TriaseUGD\AlatTransportasi;
+
+use App\Core\Database\Template;
+use App\Core\Database\Type as T;
+
+class CreateAlatTransportasiTable extends Template
+{
+    public function __construct(){
+        parent::__construct(
+            'triase_ugd',
+            'alat_transportasi',
+            [
+                'id_transportasi'          => T::ID8(),
+                'nama_transportasi'        => T::TEXT(),
+            ],
+            ['id_transportasi'],
+            [['nama_transportasi']],
+            [],
+            [],
+            true,
+            __DIR__ . '/alat_transportasi.csv'
+        );
+    }
+}
