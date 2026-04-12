ec2abe387888b2f5a15df0419791a4663d222fcd
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-13 05:22:59 +0700

    Add PlanPrimerModel

---
+++ app/Features/TriaseUGD/PlanPrimer/PlanPrimerModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\TriaseUGD\PlanPrimer;
+
+use App\Core\ModelTemplate;
+
+class PlanPrimerModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'triase_ugd',
+            'plan_primer',
+            'id_plan_primer',
+            [
+                'id_plan_primer' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_plan_primer' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
