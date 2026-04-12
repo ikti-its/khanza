566cac206ec5d02e7e3a93fb57a985f8b7cad3ca
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-13 04:06:57 +0700

    Add TriaseMacamKasusModel

---
+++ app/Features/TriaseUGD/TriaseMacamKasus/TriaseMacamKasusModel.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\TriaseUGD\TriaseMacamKasus;
+
+use App\Core\ModelTemplate;
+
+class TriaseMacamKasusModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'BASE',
+            'triase_ugd',
+            'triase_macam_kasus',
+            'id_macam_kasus',
+            [
+                'id_macam_kasus' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'kode_macam_kasus' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_macam_kasus' => [
+                    'allowed' => true,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
