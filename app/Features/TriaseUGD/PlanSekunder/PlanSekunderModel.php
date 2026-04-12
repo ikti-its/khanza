73f9e013f39ba2025c9ab6a3fdd2c9dae6303147
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-13 06:11:11 +0700

    Add PlanSekunderModel

---
+++ app/Features/TriaseUGD/PlanSekunder/PlanSekunderModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\TriaseUGD\PlanSekunder;
+
+use App\Core\ModelTemplate;
+
+class PlanSekunderModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'triase_ugd',
+            'plan_sekunder',
+            'id_plan_sekunder',
+            [
+                'id_plan_sekunder' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'nama_plan_sekunder' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
