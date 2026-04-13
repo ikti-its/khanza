018fc3c35b266c6f5add7c210b66f3472a69d646
qoyyimil <qoyyimillj@gmail.com>
2026-04-13 17:22:48 +0700

    Add RefKategoriUsiaLabController

---
+++ app/Features/Laboratorium/RefKategoriUsiaLab/RefKategoriUsiaLabController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Laboratorium\RefKategoriUsiaLab;
+use App\Core\Controller\ControllerTemplate;
+
+class RefKategoriUsiaLabController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefKategoriUsiaLabModel(),
+            breadcrumbs: [
+                ['title' => 'Laboratorium', 'icon' => 'laboratorium'],
+                ['title' => 'Referensi Kategori Usia Lab', 'icon' => 'ref_kategori_usia_lab'],
+            ],
+            judul: 'Referensi Kategori Usia Lab',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Kategori Usia',   'id_kategori_usia',   'indeks', 0],
+                [1, 'Nama Kategori Usia', 'nama_kategori_usia', 'teks',   1],
+            ],
+        );
+    }
+}
