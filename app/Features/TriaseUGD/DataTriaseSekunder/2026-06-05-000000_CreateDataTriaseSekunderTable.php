f94cbebc00dcd0cdf317a2bdea1c6ea7c7c1be2d
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-13 07:25:20 +0700

    Add feature Data Triase Sekunder in Triase UGD

---
+++ app/Features/TriaseUGD/DataTriaseSekunder/2026-06-05-000000_CreateDataTriaseSekunderTable.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\TriaseUGD\DataTriaseSekunder;
+
+use App\Core\Database\Template;
+use App\Core\Database\Type as T;
+
+class CreateDataTriaseSekunderTable extends Template
+{
+    public function __construct(){
+        parent::__construct(
+            'triase_ugd',
+            'data_triase_sekunder',
+            [
+                'id_triase_sekunder'        => T::ID32(),
+                'id_triase'                 => T::INT32(),
+                'anamnesa_singkat'          => T::TEXT(),
+                'catatan'                   => T::TEXT(),
+                'id_plan_sekunder'          => T::INT8(),
+                'tanggal_triase'            => T::DATETIME(),
+                'id_petugas'                => T::UUID(),
+            ],
+            ['id_triase_sekunder'],
+            [['id_triase']],
+            [
+                [['id_triase'], 'data_triase', ['id_triase'], 'CASCADE', 'CASCADE'],
+                [['id_plan_sekunder'], 'plan_sekunder', ['id_plan_sekunder'], 'CASCADE', 'CASCADE'],
+                // [['id_petugas'], 'sik.pegawai_structure', ['id'], 'CASCADE', 'CASCADE'],
+            ],
+            [],
+        );
+    }
+}
