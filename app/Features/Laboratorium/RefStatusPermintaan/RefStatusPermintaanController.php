60863887bbbb026c4cfd1ad553e403b3d3c6ba01
qoyyimil <qoyyimillj@gmail.com>
2026-04-13 17:23:05 +0700

    Add RefStatusPermintaanController

---
+++ app/Features/Laboratorium/RefStatusPermintaan/RefStatusPermintaanController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Laboratorium\RefStatusPermintaan;
+use App\Core\Controller\ControllerTemplate;
+
+class RefStatusPermintaanController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefStatusPermintaanModel(),
+            breadcrumbs: [
+                ['title' => 'Laboratorium', 'icon' => 'laboratorium'],
+                ['title' => 'Referensi Status Permintaan', 'icon' => 'ref_status_permintaan'],
+            ],
+            judul: 'Referensi Status Permintaan',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Status',   'id_status',   'indeks', 0],
+                [1, 'Nama Status', 'nama_status', 'teks',   1],
+            ],
+        );
+    }
+}
