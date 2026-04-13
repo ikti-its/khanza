e474216aad84504d0db1c6bb8c735854ffa47121
qoyyimil <qoyyimillj@gmail.com>
2026-04-13 17:22:43 +0700

    Add RefKategoriLabController

---
+++ app/Features/Laboratorium/RefKategoriLab/RefKategoriLabController.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Laboratorium\RefKategoriLab;
+use App\Core\Controller\ControllerTemplate;
+
+class RefKategoriLabController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefKategoriLabModel(),
+            breadcrumbs: [
+                ['title' => 'Laboratorium', 'icon' => 'laboratorium'],
+                ['title' => 'Referensi Kategori Lab', 'icon' => 'ref_kategori_lab'],
+            ],
+            judul: 'Referensi Kategori Lab',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Kategori',   'id_kategori',   'indeks', 0],
+                [1, 'Kode Kategori', 'kode_kategori', 'teks',   1],
+                [1, 'Nama Kategori', 'nama_kategori', 'teks',   1],
+            ],
+        );
+    }
+}
