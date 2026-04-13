89a9d18fb3d9fd53df7d148a30313bcb40237673
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-13 17:38:19 +0700

    Add RhesusController

---
+++ app/Features/Darah/Rhesus/RhesusController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Darah\Rhesus;
+use App\Core\Controller\ControllerTemplate;
+
+class RhesusController extends ControllerTemplate
+{
+    public function __construct(
+    ){
+        parent::__construct(
+            model: new RhesusModel(),
+            breadcrumbs: [
+                ['title' => 'Darah', 'icon' => 'darah'],
+                ['title' => 'Rhesus', 'icon' => 'rhesus'],
+            ],
+            judul: 'Rhesus',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false, 
+                'hapus'  => false
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required, *opsi
+                [0, 'ID Rhesus', 'id_rhesus', 'indeks', 0],
+                [1, 'Kode Rhesus', 'kode_rhesus', 'teks', 1],
+            ],
+        );
+    }   
+}
