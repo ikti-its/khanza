ca5543f91786291dec64041cb0c777f4e34001d5
qoyyimil <qoyyimillj@gmail.com>
2026-04-14 08:51:40 +0700

    Add RefSkriningBatukController

---
+++ app/Features/RawatJalan/SkriningRJ/RefSkriningBatuk/RefSkriningBatukController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\RawatJalan\SkriningRJ\RefSkriningBatuk;
+use App\Core\Controller\ControllerTemplate;
+
+class RefSkriningBatukController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefSkriningBatukModel(),
+            breadcrumbs: [
+                ['title' => 'Rawat Jalan', 'icon' => 'rawat_jalan'],
+                ['title' => 'Referensi Skrining Batuk', 'icon' => 'ref_skrining_batuk'],
+            ],
+            judul: 'Referensi Skrining Batuk',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Batuk',      'id_batuk',       'indeks', 0],
+                [1, 'Kategori Batuk','kategori_batuk', 'teks',   1],
+            ],
+        );
+    }
+}
