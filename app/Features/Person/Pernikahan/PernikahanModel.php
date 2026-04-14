e7e032731e40f6e6d2633f07bef2144673358f0f
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-14 14:24:41 +0700

    Add PernikahanModel

---
+++ app/Features/Person/Pernikahan/PernikahanModel.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Person\Pernikahan;
+
+use App\Core\ModelTemplate;
+
+class PernikahanModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'REFS',
+            'person',
+            'pernikahan',
+            'id_pernikahan',
+            [
+                'id_pernikahan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ],
+                'status_pernikahan' => [
+                    'allowed' => false,
+                    'rules'   => '',
+                    'errors'  => [],
+                ]
+            ],
+        );
+    }
+}
