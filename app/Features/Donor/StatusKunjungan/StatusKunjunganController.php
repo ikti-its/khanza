290b22046c8e94b1bbc0f0db28b88fa7f49f635a
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-14 16:00:28 +0700

    Add StatusKunjunganController

---
+++ app/Features/Donor/StatusKunjungan/StatusKunjunganController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Donor\StatusKunjungan;
+use App\Core\Controller\ControllerTemplate;
+
+class StatusKunjunganController extends ControllerTemplate
+{
+    public function __construct(
+    ){
+        parent::__construct(
+            model: new StatusKunjunganModel(),
+            breadcrumbs: [
+                ['title' => 'Donor', 'icon' => 'donor'],
+                ['title' => 'Status Kunjungan', 'icon' => 'status_kunjungan'],
+            ],
+            judul: 'Status Kunjungan',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false, 
+                'hapus'  => false
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required, *opsi
+                [0, 'ID Status Kunjungan', 'id_status_kunjungan', 'indeks', 0],
+                [1, 'Nama Status Kunjungan', 'nama_status_kunjungan', 'teks', 1],
+            ],
+        );
+    }   
+}
