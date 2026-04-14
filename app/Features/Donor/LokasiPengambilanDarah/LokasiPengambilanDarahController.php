06eb851ea1da483e35fdb19814f10959db524239
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-14 16:10:10 +0700

    Add LokasiPengambilanDarahController

---
+++ app/Features/Donor/LokasiPengambilanDarah/LokasiPengambilanDarahController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Donor\LokasiPengambilanDarah;
+use App\Core\Controller\ControllerTemplate;
+
+class LokasiPengambilanDarahController extends ControllerTemplate
+{
+    public function __construct(
+    ){
+        parent::__construct(
+            model: new LokasiPengambilanDarahModel(),
+            breadcrumbs: [
+                ['title' => 'Donor', 'icon' => 'donor'],
+                ['title' => 'Lokasi Pengambilan Darah', 'icon' => 'lokasi_pengambilan_darah'],
+            ],
+            judul: 'Lokasi Pengambilan Darah',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false, 
+                'hapus'  => false
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required, *opsi
+                [0, 'ID Lokasi Pengambilan', 'id_lokasi_pengambilan', 'indeks', 0],
+                [1, 'Nama Lokasi', 'nama_lokasi', 'teks', 1],
+            ],
+        );
+    }   
+}
