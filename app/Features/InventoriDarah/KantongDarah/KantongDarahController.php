10506aa7992c7d62b30bd20db87ed12e33779e57
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-15 22:18:39 +0700

    Add KantongDarahController

---
+++ app/Features/InventoriDarah/KantongDarah/KantongDarahController.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\InventoriDarah\KantongDarah;
+use App\Core\Controller\ControllerTemplate;
+
+class KantongDarahController extends ControllerTemplate
+{
+    public function __construct(
+    ){
+        parent::__construct(
+            model: new KantongDarahModel(),
+            breadcrumbs: [
+                ['title' => 'Inventaris Darah', 'icon' => 'inventaris_darah'],
+                ['title' => 'Kantong Darah', 'icon' => 'kantong_darah'],
+            ],
+            judul: 'Kantong Darah',
+            aksi: [
+                'tambah' => true,
+                'audit'  => true,
+                'ubah'   => true, 
+                'hapus'  => true
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required, *opsi
+                [0, 'ID Bag', 'id_bag', 'indeks', 0],
+                [1, 'ID Pengambilan Darah', 'id_pengambilan_darah', 'indeks', 1],
+                [1, 'Nomor Bag', 'no_bag', 'teks', 1],
+                [1, 'ID Jenis Bag', 'id_jenis_bag', 'indeks', 1],
+            ],
+        );
+    }   
+}
