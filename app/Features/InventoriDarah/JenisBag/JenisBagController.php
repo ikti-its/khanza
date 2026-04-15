96a64c42197c74b923ce76ebf19c8fb1276cd949
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-15 18:18:34 +0700

    Add JenisBagController

---
+++ app/Features/InventoriDarah/JenisBag/JenisBagController.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\InventoriDarah\JenisBag;
+use App\Core\Controller\ControllerTemplate;
+
+class JenisBagController extends ControllerTemplate
+{
+    public function __construct(
+    ){
+        parent::__construct(
+            model: new JenisBagModel(),
+            breadcrumbs: [
+                ['title' => 'Inventaris Darah', 'icon' => 'inventaris_darah'],
+                ['title' => 'Jenis Bag', 'icon' => 'jenis_bag'],
+            ],
+            judul: 'Jenis Bag',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false, 
+                'hapus'  => false
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required, *opsi
+                [0, 'ID Jenis Bag', 'id_jenis_bag', 'indeks', 0],
+                [1, 'Kode', 'kode_jenis_bag', 'teks', 1],
+                [1, 'Nama Jenis Bag', 'nama_jenis_bag', 'teks', 1],
+            ],
+        );
+    }   
+}
