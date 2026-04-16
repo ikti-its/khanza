5b681fafde359f9ae3b1acab1fb7612d6399d4e7
qoyyimil <qoyyimillj@gmail.com>
2026-04-16 21:16:06 +0700

    Add RefJenisSedasiController

---
+++ app/Features/Operasi/RefJenisSedasi/RefJenisSedasiController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefJenisSedasi;
+use App\Core\Controller\ControllerTemplate;
+
+class RefJenisSedasiController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefJenisSedasiModel(),
+            breadcrumbs: [
+                ['title' => 'Operasi', 'icon' => 'operasi'],
+                ['title' => 'Referensi Jenis Sedasi', 'icon' => 'ref_jenis_sedasi'],
+            ],
+            judul: 'Referensi Jenis Sedasi',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Jenis Sedasi', 'id_jenis_sedasi', 'indeks', 0],
+                [1, 'Nama Sedasi',     'nama_sedasi',     'teks',   1],
+            ],
+        );
+    }
+}
