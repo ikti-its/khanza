384b1378f580a67fc0f420a105df51a4bc2fbb40
qoyyimil <qoyyimillj@gmail.com>
2026-04-16 21:14:52 +0700

    Add RefAldretteAktivitasController

---
+++ app/Features/Operasi/RefAldretteAktivitas/RefAldretteAktivitasController.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefAldretteAktivitas;
+use App\Core\Controller\ControllerTemplate;
+
+class RefAldretteAktivitasController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefAldretteAktivitasModel(),
+            breadcrumbs: [
+                ['title' => 'Operasi', 'icon' => 'operasi'],
+                ['title' => 'Referensi Aldrette Aktivitas', 'icon' => 'ref_aldrette_aktivitas'],
+            ],
+            judul: 'Referensi Aldrette Aktivitas',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Aktivitas', 'id_aktivitas', 'indeks', 0],
+                [1, 'Nama Skala',   'nama_skala',   'teks',   1],
+                [1, 'Nilai',        'nilai',        'jumlah', 1],
+            ],
+        );
+    }
+}
