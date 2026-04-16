5244196f1fe68d5d71ccb6e7703e314c4d488ce3
qoyyimil <qoyyimillj@gmail.com>
2026-04-16 21:15:15 +0700

    Add RefAldretteTekananDarahController

---
+++ app/Features/Operasi/RefAldretteTekananDarah/RefAldretteTekananDarahController.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefAldretteTekananDarah;
+use App\Core\Controller\ControllerTemplate;
+
+class RefAldretteTekananDarahController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefAldretteTekananDarahModel(),
+            breadcrumbs: [
+                ['title' => 'Operasi', 'icon' => 'operasi'],
+                ['title' => 'Referensi Aldrette Tekanan Darah', 'icon' => 'ref_aldrette_tekanan_darah'],
+            ],
+            judul: 'Referensi Aldrette Tekanan Darah',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Tekanan Darah', 'id_td',      'indeks', 0],
+                [1, 'Nama Skala',       'nama_skala', 'teks',   1],
+                [1, 'Nilai',            'nilai',      'jumlah', 1],
+            ],
+        );
+    }
+}
