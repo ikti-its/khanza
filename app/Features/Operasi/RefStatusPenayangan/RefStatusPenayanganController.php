283a86c07d09d6a04db8f9c3df226fe96f7a8ddf
qoyyimil <qoyyimillj@gmail.com>
2026-04-16 21:19:50 +0700

    Add RefStatusPenayanganController

---
+++ app/Features/Operasi/RefStatusPenayangan/RefStatusPenayanganController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefStatusPenayangan;
+use App\Core\Controller\ControllerTemplate;
+
+class RefStatusPenayanganController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefStatusPenayanganModel(),
+            breadcrumbs: [
+                ['title' => 'Operasi', 'icon' => 'operasi'],
+                ['title' => 'Referensi Status Penayangan', 'icon' => 'ref_status_penayangan'],
+            ],
+            judul: 'Referensi Status Penayangan',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Status Penayangan', 'id_status_penayangan', 'indeks', 0],
+                [1, 'Nama Status',          'nama_status',          'teks',   1],
+            ],
+        );
+    }
+}
