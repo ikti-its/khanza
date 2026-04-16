4c13031bac692c8843de10b6fe6c7cbd04cd2898
qoyyimil <qoyyimillj@gmail.com>
2026-04-16 21:20:14 +0700

    Add RefStewardRespirasiController

---
+++ app/Features/Operasi/RefStewardRespirasi/RefStewardRespirasiController.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefStewardRespirasi;
+use App\Core\Controller\ControllerTemplate;
+
+class RefStewardRespirasiController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefStewardRespirasiModel(),
+            breadcrumbs: [
+                ['title' => 'Operasi', 'icon' => 'operasi'],
+                ['title' => 'Referensi Steward Respirasi', 'icon' => 'ref_steward_respirasi'],
+            ],
+            judul: 'Referensi Steward Respirasi',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Respirasi', 'id_respirasi', 'indeks', 0],
+                [1, 'Nama Skala',   'nama_skala',   'teks',   1],
+                [1, 'Nilai',        'nilai',        'jumlah', 1],
+            ],
+        );
+    }
+}
