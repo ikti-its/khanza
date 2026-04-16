04408526ab04e93a221a591fa213a468a21d6268
qoyyimil <qoyyimillj@gmail.com>
2026-04-16 21:18:54 +0700

    Add RefMetodeTransferController

---
+++ app/Features/Operasi/RefMetodeTransfer/RefMetodeTransferController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefMetodeTransfer;
+use App\Core\Controller\ControllerTemplate;
+
+class RefMetodeTransferController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefMetodeTransferModel(),
+            breadcrumbs: [
+                ['title' => 'Operasi', 'icon' => 'operasi'],
+                ['title' => 'Referensi Metode Transfer', 'icon' => 'ref_metode_transfer'],
+            ],
+            judul: 'Referensi Metode Transfer',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Metode',   'id_metode',   'indeks', 0],
+                [1, 'Nama Metode', 'nama_metode', 'teks',   1],
+            ],
+        );
+    }
+}
