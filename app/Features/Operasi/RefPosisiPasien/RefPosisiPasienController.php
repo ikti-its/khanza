b5346f402d933a802b26be229de1b13b684a3d54
qoyyimil <qoyyimillj@gmail.com>
2026-04-16 21:19:09 +0700

    Add RefPosisiPasienController

---
+++ app/Features/Operasi/RefPosisiPasien/RefPosisiPasienController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefPosisiPasien;
+use App\Core\Controller\ControllerTemplate;
+
+class RefPosisiPasienController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefPosisiPasienModel(),
+            breadcrumbs: [
+                ['title' => 'Operasi', 'icon' => 'operasi'],
+                ['title' => 'Referensi Posisi Pasien', 'icon' => 'ref_posisi_pasien'],
+            ],
+            judul: 'Referensi Posisi Pasien',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Posisi',   'id_posisi',   'indeks', 0],
+                [1, 'Nama Posisi', 'nama_posisi', 'teks',   1],
+            ],
+        );
+    }
+}
