f0b2b755c2d587644b61cd7fc0f80dd173514d84
qoyyimil <qoyyimillj@gmail.com>
2026-04-16 21:18:59 +0700

    Add RefObatBebasController

---
+++ app/Features/Operasi/RefObatBebas/RefObatBebasController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefObatBebas;
+use App\Core\Controller\ControllerTemplate;
+
+class RefObatBebasController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefObatBebasModel(),
+            breadcrumbs: [
+                ['title' => 'Operasi', 'icon' => 'operasi'],
+                ['title' => 'Referensi Obat Bebas', 'icon' => 'ref_obat_bebas'],
+            ],
+            judul: 'Referensi Obat Bebas',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Obat Bebas',  'id_obat_bebas',  'indeks', 0],
+                [1, 'Nama Kategori',  'nama_kategori',  'teks',   1],
+            ],
+        );
+    }
+}
