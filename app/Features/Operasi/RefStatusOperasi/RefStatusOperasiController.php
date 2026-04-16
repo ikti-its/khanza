5d1d9e00e99ff7ecb32a82212dd5a5c327bae171
qoyyimil <qoyyimillj@gmail.com>
2026-04-16 21:19:44 +0700

    Add RefStatusOperasiController

---
+++ app/Features/Operasi/RefStatusOperasi/RefStatusOperasiController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefStatusOperasi;
+use App\Core\Controller\ControllerTemplate;
+
+class RefStatusOperasiController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefStatusOperasiModel(),
+            breadcrumbs: [
+                ['title' => 'Operasi', 'icon' => 'operasi'],
+                ['title' => 'Referensi Status Operasi', 'icon' => 'ref_status_operasi'],
+            ],
+            judul: 'Referensi Status Operasi',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Status',   'id_status',   'indeks', 0],
+                [1, 'Nama Status', 'nama_status', 'teks',   1],
+            ],
+        );
+    }
+}
