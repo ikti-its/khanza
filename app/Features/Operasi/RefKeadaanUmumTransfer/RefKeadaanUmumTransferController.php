7caf781887365620b19d955be2bac4baabb1d94d
qoyyimil <qoyyimillj@gmail.com>
2026-04-16 21:16:28 +0700

    Add RefKeadaanUmumTransferController

---
+++ app/Features/Operasi/RefKeadaanUmumTransfer/RefKeadaanUmumTransferController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefKeadaanUmumTransfer;
+use App\Core\Controller\ControllerTemplate;
+
+class RefKeadaanUmumTransferController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefKeadaanUmumTransferModel(),
+            breadcrumbs: [
+                ['title' => 'Operasi', 'icon' => 'operasi'],
+                ['title' => 'Referensi Keadaan Umum Transfer', 'icon' => 'ref_keadaan_umum_transfer'],
+            ],
+            judul: 'Referensi Keadaan Umum Transfer',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Keadaan Umum', 'id_keadaan_umum', 'indeks', 0],
+                [1, 'Nama Keadaan',    'nama_keadaan',    'teks',   1],
+            ],
+        );
+    }
+}
