f86a4399b9a7f1e5ba12f879d54a95f5230dbddf
qoyyimil <qoyyimillj@gmail.com>
2026-04-16 21:19:25 +0700

    Add RefRencanaAnestesiController

---
+++ app/Features/Operasi/RefRencanaAnestesi/RefRencanaAnestesiController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefRencanaAnestesi;
+use App\Core\Controller\ControllerTemplate;
+
+class RefRencanaAnestesiController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            model: new RefRencanaAnestesiModel(),
+            breadcrumbs: [
+                ['title' => 'Operasi', 'icon' => 'operasi'],
+                ['title' => 'Referensi Rencana Anestesi', 'icon' => 'ref_rencana_anestesi'],
+            ],
+            judul: 'Referensi Rencana Anestesi',
+            aksi: [
+                'tambah' => false,
+                'audit'  => false,
+                'ubah'   => false,
+                'hapus'  => false,
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required
+                [0, 'ID Rencana Anestesi', 'id_rencana_anestesi', 'indeks', 0],
+                [1, 'Nama Rencana',        'nama_rencana',        'teks',   1],
+            ],
+        );
+    }
+}
