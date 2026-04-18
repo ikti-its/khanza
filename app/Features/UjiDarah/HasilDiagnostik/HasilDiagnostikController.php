e88718cac95bd8ea70c5f7456ea54c1b223e59be
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-18 07:59:58 +0700

    Add HasilDiagnostikController

---
+++ app/Features/UjiDarah/HasilDiagnostik/HasilDiagnostikController.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UjiDarah\HasilDiagnostik;
+use App\Core\Controller\ControllerTemplate;
+
+class HasilDiagnostikController extends ControllerTemplate
+{
+    public function __construct(
+    ){
+        parent::__construct(
+            model: new HasilDiagnostikModel(),
+            breadcrumbs: [
+                ['title' => 'Uji Darah', 'icon' => 'uji_darah'],
+                ['title' => 'Hasil Diagnostik', 'icon' => 'hasil_diagnostik'],
+            ],
+            judul: 'Hasil Diagnostik',
+            aksi: [
+                'tambah' => true,
+                'audit'  => true,
+                'ubah'   => true,
+                'hapus'  => true
+            ],
+            konfig: [
+                //visible, display, kolom, jenis, required, *opsi
+                [0, 'ID Diagnostik', 'id_diagnostik', 'indeks', 0],
+                [1, 'ID Rujukan', 'id_rujukan', 'indeks', 1],
+                [1, 'Tanggal Hasil', 'tanggal_hasil', 'tanggal', 1],
+                [1, 'Dokter Pemeriksa', 'dokter_pemeriksa', 'nama', 1],
+            ],
+        );
+    }   
+}
