7ada38f57e91aa589fdafa0b567a23e3385e7698
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-19 09:20:08 +0700

    Feat: add datetime type to table components

--- app/Views/components/tabel/td.php
+++ app/Views/components/tabel/td.php
@@ -1,5 +1,5 @@
 <?php 
-    $list_jenis = ['indeks', 'tanggal', 'jam', 'uang', 'status', 'nama', 'teks', 'jumlah', 'suhu', 'bool', 'desimal'];
+    $list_jenis = ['indeks', 'tanggal', 'jam', 'uang', 'status', 'nama', 'teks', 'jumlah', 'suhu', 'bool', 'desimal', 'tanggal_jam'];
     for($i = 0; $i < sizeof($kolom); $i++){
         if(!in_array($jenis[$i], $list_jenis)){
             echo 'Jenis tidak ditemukan pada daftar';
