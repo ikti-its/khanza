c636a29ce35100cf4d333d63372a4865ca59a45a
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-20 20:29:28 +0700

    Refactor Migration in Operasional

--- app/Features/Operasional/Shift/2026-03-29-000000_CreateShiftTable.php
+++ app/Features/Operasional/Shift/2026-03-29-000000_CreateShiftTable.php
@@ -6,7 +6,7 @@ namespace App\Features\Operasional\Shift;
 use App\Core\Database\DatabaseTemplate;
 use App\Core\Database\DatabaseType as T;
 
-class CreateShiftTable extends DatabaseTemplate
+final class CreateShiftTable extends DatabaseTemplate
 {
     public function __construct(){
         parent::__construct(
@@ -16,8 +16,8 @@ class CreateShiftTable extends DatabaseTemplate
                 'id_shift'         => T::ID8(),
                 'nama_shift'       => T::TEXT(),
             ],
-            ['id_shift'],
-            [['nama_shift']],
+            'id_shift',
+            'nama_shift',
             [],
             true,
             __DIR__ . '/shift.csv'
