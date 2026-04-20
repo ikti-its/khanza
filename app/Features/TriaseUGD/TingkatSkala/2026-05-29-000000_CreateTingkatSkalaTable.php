2bff20cc19b04c410030079069911c01a4084cdb
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-20 09:23:40 +0700

    Add feature Tingkat Skala in Triase UGD

---
+++ app/Features/TriaseUGD/TingkatSkala/2026-05-29-000000_CreateTingkatSkalaTable.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\TriaseUGD\TingkatSkala;
+
+use App\Core\Database\DatabaseTemplate;
+use App\Core\Database\DatabaseType as T;
+
+class CreateTingkatSkalaTable extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'triase_ugd',
+            'tingkat_skala',
+            [
+                'id_tingkat'          => T::ID8(),
+                'nama_tingkat'        => T::TEXT(),
+            ],
+            ['id_tingkat'],
+            [['nama_tingkat']],
+            [],
+            [],
+            true,
+            __DIR__ . '/tingkat_skala.csv'
+        );
+    }
+}
