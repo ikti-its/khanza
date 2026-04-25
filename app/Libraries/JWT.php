556e4d65f8f6c54b0969cb8dac86cc7ad97bab40
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-04-25 14:54:49 +0700

    Delete JWT library because it is already in /vendor

--- app/Libraries/JWT.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace App\Libraries;
-
-// Include the library file
-require_once APPPATH . 'Libraries/Jwt/JWT.php';
-
-use \Firebase\JWT\JWT as FirebaseJWT;
-
-class JWT extends FirebaseJWT {}
