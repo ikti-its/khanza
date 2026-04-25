c5e037a9d3c937f812eab071f19b76a470d499fa
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-25 21:54:02 +0700

    Update BHPRusakController

--- app/Features/LogistikUTD/BHPRusak/BHPRusakController.php
+++ app/Features/LogistikUTD/BHPRusak/BHPRusakController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Features\LogistikUTD\BHPRusak;
 use App\Core\Controller\ControllerTemplate;
+use App\Core\Controller\InputType as I;
 
 final class BHPRusakController extends ControllerTemplate
 {
@@ -22,15 +23,15 @@ final class BHPRusakController extends ControllerTemplate
             ],
             konfig: [
                 //visible, display, kolom, jenis, required, *opsi
-                [HIDE, 'ID BHP Rusak', 'id_bhp_rusak', 'indeks', OPTIONAL],
-                [SHOW, 'ID Jenis BHP', 'id_jenis_bhp', 'indeks', REQUIRED],
-                [SHOW, 'ID Barang Medis', 'id_barang_medis', 'indeks', OPTIONAL],
-                [SHOW, 'ID Barang Non Medis', 'id_barang_penunjang', 'indeks', OPTIONAL],
-                [SHOW, 'Jumlah', 'jumlah', 'jumlah', REQUIRED],
-                [SHOW, 'Harga Beli', 'harga_beli', 'uang', REQUIRED],
-                [HIDE, 'ID Petugas', 'id_petugas', 'indeks', REQUIRED],
+                [HIDE, 'ID BHP Rusak', 'id_bhp_rusak', I::INDEX, OPTIONAL],
+                [SHOW, 'ID Jenis BHP', 'id_jenis_bhp', I::INDEX, REQUIRED],
+                [SHOW, 'ID Barang Medis', 'id_barang_medis', I::INDEX, OPTIONAL],
+                [SHOW, 'ID Barang Non Medis', 'id_barang_penunjang', I::INDEX, OPTIONAL],
+                [SHOW, 'Jumlah', 'jumlah', I::NUMBER, REQUIRED],
+                [SHOW, 'Harga Beli', 'harga_beli', I::MONEY, REQUIRED],
+                [HIDE, 'ID Petugas', 'id_petugas', I::INDEX, REQUIRED],
                 [SHOW, 'Tanggal Rusak', 'tanggal_rusak', 'tanggal_jam', REQUIRED],
-                [SHOW, 'Keterangan', 'keterangan', 'teks', REQUIRED],
+                [SHOW, 'Keterangan', 'keterangan', I::TEXT, REQUIRED],
             ],
         );
     }   
