6628c217b971d6109343d139061e556f9161df9b
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-26 17:51:41 +0700

    Add ShiftController

---
+++ app/Features/Operasional/Shift/ShiftController.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasional\Shift;
+use App\Core\Controller\ControllerTemplate;
+use App\Core\Controller\InputType as I;
+use App\Core\Controller\ActionType as A;
+
+final class ShiftController extends ControllerTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            new ShiftModel(),
+            [
+                ['Operasional', 'operasional'],
+                ['Shift', 'shift'],
+            ],
+            'Shift',
+            [
+                // A::CREATE,
+                // A::AUDIT,
+                // A::UPDATE, 
+                // A::DELETE,
+            ],
+            [
+                [HIDE, OPTIONAL, I::INDEX, 'id_shift', 'ID Shift'],
+                [SHOW, REQUIRED, I::TEXT, 'nama_shift', 'Nama Shift'],
+            ],
+        );
+    }   
+}
