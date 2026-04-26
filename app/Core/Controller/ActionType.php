19819d48fed1a975e62bc17097efa7e0749066bc
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-04-26 15:24:55 +0700

    Add ActionType enum in Controller

---
+++ app/Core/Controller/ActionType.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Core\Controller;
+
+enum ActionType: string
+{
+    case CREATE = 'tambah';
+    case UPDATE = 'ubah';
+    case DELETE = 'hapus';
+    case AUDIT  = 'audit';
+}
