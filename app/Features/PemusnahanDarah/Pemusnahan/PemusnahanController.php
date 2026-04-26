a52ecf2a5d9cd27fb1c6a7fcde8dc7db4b44ed10
Aliyah <aliyahawwaliyah@gmail.com>
2026-04-26 16:22:50 +0700

    Add ActionType import in PemusnahanController

--- app/Features/PemusnahanDarah/Pemusnahan/PemusnahanController.php
+++ app/Features/PemusnahanDarah/Pemusnahan/PemusnahanController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Features\PemusnahanDarah\Pemusnahan;
 use App\Core\Controller\ControllerTemplate;
 use App\Core\Controller\InputType as I;
+use App\Core\Controller\ActionType as A;
 
 final class PemusnahanController extends ControllerTemplate
 {
