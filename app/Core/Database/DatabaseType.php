d2128447f5eba09513bcd5a5fe55fa5149747887
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-05-01 15:52:14 +0700

    Add ID checks, FK_AUTO, remove 24 bit ID and INT, and reformat DatabaseType

--- app/Core/Database/DatabaseType.php
+++ app/Core/Database/DatabaseType.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 namespace App\Core\Database;
+use App\Core\Controller\Assert;
 use CodeIgniter\Database\RawSql;
 
 final class DatabaseType
@@ -13,77 +14,110 @@ final class DatabaseType
         $this->definition['null'] ??= false;
     }
 
-    public function definition(): array
-    {
-        return $this->definition;
-    }
+    public function definition(): array { 
+        return $this->definition;}
 
-    public function nullable(): self
-    {
-        return new self([
-            ...$this->definition,
-            'null' => true,
-        ]);
-    }
+    public function nullable(): self {
+        return new self([...$this->definition,'null' => true]);}
 
-    public function default(mixed $value): self
-    {
-        return new self([
-            ...$this->definition,
-            'default' => $value,
-        ]);
-    }
+    public function default(mixed $value): self { 
+        return new self([...$this->definition,'default' => $value]);}
 
-    public function defaultRaw(string $sql): self
-    {
-        return new self([
-            ...$this->definition,
-            'default' => new RawSql($sql),
-        ]);
-    }
+    public function defaultRaw(string $sql): self {
+        return new self([...$this->definition,
+            'default' => new RawSql($sql)]);}
 
-    public function check(string $expr): self
-    {
-        return new self([
-            ...$this->definition,
-            'check' => $expr,
-        ]);
-    }
+    public function check(string $expr): self {
+        return new self([...$this->definition,'check' => $expr]);}
     
     public static function UUID(): self {
-        return new self([
-            'type' => 'UUID',
-            'default' => new RawSql('gen_random_uuid()'),
-        ]);
+        return new self(['type' => 'UUID', 
+            'default' => new RawSql('gen_random_uuid()')]);}
+
+    public static function FK_AUTO() : self { 
+        return self::UUID();}
+
+    public static function ID8(int $max = 0) : self { 
+        Assert::True($max > (2 << 31), 
+        "Maximum value $max higher than 2^31, please use ID64");
+        Assert::True($max > (2 << 15), 
+        "Maximum value $max higher than 2^15, please use ID32");
+        Assert::True($max > (2 << 7), 
+        "Maximum value $max higher than 2^7, please use ID16");
+
+        return new self(['type' => 'SMALLINT', 'auto_increment' => true]);
+    }
+    public static function ID16(int $max = 0): self { 
+        Assert::True($max > (2 << 31), 
+        "Maximum value $max higher than 2^31, please use ID64");
+        Assert::True($max > (2 << 15), 
+        "Maximum value $max higher than 2^15, please use ID32");
+         Assert::True($max < (2 << 7), 
+        "Maximum value $max lower than 2^7, please use ID8");
+
+        return new self(['type' => 'SMALLINT', 'auto_increment' => true]);
+    }
+    public static function ID32(int $max = 0): self {
+        Assert::True($max > (2 << 31), 
+        "Maximum value $max higher than 2^31, please use ID64");
+        Assert::True($max < (2 << 7), 
+        "Maximum value $max lower than 2^7, please use ID8");
+        Assert::True($max < (2 << 15), 
+        "Maximum value $max lower than 2^15, please use ID16");
+        
+        return new self(['type' => 'INTEGER', 'auto_increment' => true]);
+    }
+    public static function ID64(int $max = 0): self { 
+        Assert::True($max < (2 << 7), 
+        "Maximum value $max lower than 2^7, please use ID8");
+        Assert::True($max < (2 << 15), 
+        "Maximum value $max lower than 2^15, please use ID16");
+        Assert::True($max < (2 << 31), 
+        "Maximum value $max lower than 2^31, please use ID32");
+
+        return new self(['type' => 'BIGINT', 'auto_increment' => true]);
     }
 
-    public static function ID8() : self { return new self(['type' => 'SMALLINT', 'auto_increment' => true]);}
-    public static function ID16(): self { return new self(['type' => 'SMALLINT', 'auto_increment' => true]);}
-    public static function ID24(): self { return new self(['type' => 'INTEGER', 'auto_increment' => true]);}
-    public static function ID32(): self { return new self(['type' => 'INTEGER', 'auto_increment' => true]);}
-    public static function ID64(): self { return new self(['type' => 'BIGINT', 'auto_increment' => true]);}
+    
+    public static function INT8() : self { 
+        return new self(['type' => 'SMALLINT']);}
+    public static function INT16(): self { 
+        return new self(['type' => 'SMALLINT']);}
+    public static function INT32(): self { 
+        return new self(['type' => 'INTEGER']);}
+    public static function INT64(): self { 
+        return new self(['type' => 'BIGINT']);}
 
-    public static function INT8() : self { return new self(['type' => 'SMALLINT']);}
-    public static function INT16(): self { return new self(['type' => 'SMALLINT']);}
-    public static function INT24(): self { return new self(['type' => 'INTEGER']);}
-    public static function INT32(): self { return new self(['type' => 'INTEGER']);}
-    public static function INT64(): self { return new self(['type' => 'BIGINT']);}
+    public static function F32(): self { 
+        return new self(['type' => 'REAL']);}
+    public static function F64(): self { 
+        return new self(['type' => 'DOUBLE PRECISION']);}
+    public static function TEXT(): self { 
+        return new self(['type' => 'TEXT']);}
+    public static function BOOL(): self { 
+        return new self(['type' => 'BOOLEAN']);}
 
-    public static function F32(): self { return new self(['type' => 'REAL']);}
-    public static function F64(): self { return new self(['type' => 'DOUBLE PRECISION']);}
-    public static function TEXT(): self { return new self(['type' => 'TEXT']);}
-    public static function BOOL(): self { return new self(['type' => 'BOOLEAN']);}
+    public static function DATE(): self { 
+        return new self(['type' => 'DATE']);}
+    public static function TIME(): self { 
+        return new self(['type' => 'TIME']);}
+    public static function DATETIME(): self { 
+        return new self(['type' => 'TIMESTAMPTZ']);}
+    public static function YEAR(): self { 
+        return new self(['type' => 'SMALLINT',
+            'check' => 'VALUE >= 1900 AND VALUE <= 2100']);}
 
-    public static function DATE(): self { return new self(['type' => 'DATE']);}
-    public static function TIME(): self { return new self(['type' => 'TIME']);}
-    public static function DATETIME(): self { return new self(['type' => 'TIMESTAMPTZ']);}
-    public static function YEAR(): self { return new self(['type' => 'SMALLINT','check' => 'VALUE >= 1900 AND VALUE <= 2100']);}
-
-    public static function IPV4(): self { return new self(['type' => 'INET','check' => 'family(VALUE) = 4']);}
-    public static function IPV6() : self { return new self(['type' => 'INET','check' => 'family(VALUE) = 6']);}
-    public static function NETV4(): self { return new self(['type' => 'CIDR','check' => 'family(VALUE) = 4']);}
-    public static function NETV6(): self { return new self(['type' => 'CIDR','check' => 'family(VALUE) = 6']);}
-    public static function MAC48(): self { return new self(['type' => 'MACADDR']);}
-    public static function MAC64(): self { return new self(['type' => 'MACADDR8']);}
+    public static function IPV4(): self { 
+        return new self(['type' => 'INET','check' => 'family(VALUE) = 4']);}
+    public static function IPV6() : self { 
+        return new self(['type' => 'INET','check' => 'family(VALUE) = 6']);}
+    public static function NETV4(): self { 
+        return new self(['type' => 'CIDR','check' => 'family(VALUE) = 4']);}
+    public static function NETV6(): self { 
+        return new self(['type' => 'CIDR','check' => 'family(VALUE) = 6']);}
+    public static function MAC48(): self { 
+        return new self(['type' => 'MACADDR']);}
+    public static function MAC64(): self { 
+        return new self(['type' => 'MACADDR8']);}
 }
 
