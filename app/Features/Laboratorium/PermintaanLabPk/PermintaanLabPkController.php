5cf518d7dc13d5e2c1d4392c8724e1e520628844
qoyyimil <qoyyimillj@gmail.com>
2026-05-02 21:45:12 +0700

    Adjust PermintaanLabPkDatabase and add PermintaanLabPk seed

--- app/Features/Laboratorium/PermintaanLabPk/PermintaanLabPkController.php
+++ app/Features/Laboratorium/PermintaanLabPk/PermintaanLabPkController.php
@@ -21,7 +21,7 @@ final class PermintaanLabPkController extends ControllerTemplate
                 A::CREATE,
                 A::AUDIT,
                 A::UPDATE,
-                A::DELETE,,
+                A::DELETE,
             ],
             [
                 [HIDE, OPTIONAL, I::INDEX, 'id_permintaan_pk', 'ID Permintaan PK'],
---
+++ app/Features/Laboratorium/PermintaanLabPk/PermintaanLabPkDatabase.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Laboratorium\PermintaanLabPk;
+
+use App\Core\Database\DatabaseTemplate;
+use App\Core\Database\DatabaseType as T;
+
+final class PermintaanLabPkDatabase extends DatabaseTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'laboratorium',
+            'permintaan_lab_pk',
+            [
+                'id_permintaan_pk' => T::ID32(100_000),
+                'id_permintaan_lab' => T::FK_AUTO(),
+                'id_item_pemeriksaan' => T::FK_AUTO(),
+                'id_parameter_pemeriksaan' => T::FK_AUTO(),
+            ],
+            'id_permintaan_pk',
+            [],
+            [
+                [
+                    ['id_permintaan_lab'],
+                    \App\Features\Laboratorium\PermintaanLabHeader\PermintaanLabHeaderDatabase::class,
+                    ['id_permintaan'],
+                ],
+                [
+                    ['id_item_pemeriksaan'],
+                    \App\Features\Laboratorium\RefItemPemeriksaanLab\RefItemPemeriksaanLabDatabase::class,
+                    ['id_item_lab'],
+                ],
+                [
+                    ['id_parameter_pemeriksaan'],
+                    \App\Features\Laboratorium\RefParameterPemeriksaanLab\RefParameterPemeriksaanLabDatabase::class,
+                    ['id_parameter'],
+                ],
+            ],
+            false,
+            'permintaan_lab_pk.csv'
+        );
+    }
+}
