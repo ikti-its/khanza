e059c9d21900c6924fd77f8bb53a768947d33f66
qoyyimil <qoyyimillj@gmail.com>
2026-05-02 21:29:36 +0700

    Adjust HasilLabPaDatabase and add HasilLabPa seed

--- app/Features/Laboratorium/HasilLabPa/HasilLabPaController.php
+++ app/Features/Laboratorium/HasilLabPa/HasilLabPaController.php
@@ -21,7 +21,7 @@ final class HasilLabPaController extends ControllerTemplate
                 A::CREATE,
                 A::AUDIT,
                 A::UPDATE,
-                A::DELETE,,
+                A::DELETE,
             ],
             [
                 [HIDE, OPTIONAL, I::INDEX, 'id_hasil_pa', 'ID Hasil PA'],
---
+++ app/Features/Laboratorium/HasilLabPa/HasilLabPaDatabase.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Laboratorium\HasilLabPa;
+
+use App\Core\Database\DatabaseTemplate;
+use App\Core\Database\DatabaseType as T;
+
+final class HasilLabPaDatabase extends DatabaseTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'laboratorium',
+            'hasil_lab_pa',
+            [
+                'id_hasil_pa' => T::ID64(100_000),
+                'id_permintaan_lab' => T::FK_AUTO(),
+                'nomor_reg' => T::FK_AUTO(),
+                'kode_dokter_pj' => T::FK_AUTO(),
+                'id_petugas_lab' => T::FK_AUTO(),
+                'kode_dokter_perujuk' => T::TEXT(),
+                'tgl_jam_hasil' => T::DATETIME(),
+                'id_item_pemeriksaan' => T::FK_AUTO(),
+                'diagnosa_klinis' => T::TEXT(),
+                'makroskopik' => T::TEXT(),
+                'mikroskopik' => T::TEXT(),
+                'kesimpulan' => T::TEXT(),
+                'kesan' => T::TEXT()->nullable(),
+            ],
+            'id_hasil_pa',
+            [],
+            [
+                [
+                    ['id_permintaan_lab'],
+                    \App\Features\Laboratorium\PermintaanLabHeader\PermintaanLabHeaderDatabase::class,
+                    ['id_permintaan'],
+                ],
+                // ['nomor_reg', 'sik.registrasi_structure', 'nomor_reg'],
+                // ['kode_dokter_pj', 'sik.dokter_structure', 'kode_dokter'],
+                // ['id_petugas_lab', 'sik.pegawai_structure', 'id'],
+                // ['kode_dokter_perujuk', 'sik.dokter_structure', 'kode_dokter'],
+                [
+                    ['id_item_pemeriksaan'],
+                    \App\Features\Laboratorium\PermintaanLabPa\PermintaanLabPaDatabase::class,
+                    ['id_permintaan_pa'],
+                ],
+            ],
+            false,
+            'hasil_lab_pa.csv'
+        );
+    }
+}
