ec99446408c8318b5ddee47b7ee7e9abe0896fc7
qoyyimil <qoyyimillj@gmail.com>
2026-05-02 21:47:49 +0700

    Adjust RefStatusPermintaanDatabase

--- app/Features/Laboratorium/RefStatusPermintaan/RefStatusPermintaanDatabase.php
+++ app/Features/Laboratorium/RefStatusPermintaan/RefStatusPermintaanDatabase.php
@@ -1,25 +1,26 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Features\Laboratorium\RefStatusPermintaan;
 
 use App\Core\Database\DatabaseTemplate;
 use App\Core\Database\DatabaseType as T;
     
-final class CreateRefStatusPermintaanTable extends DatabaseTemplate
+final class RefStatusPermintaanDatabase extends DatabaseTemplate
 {
     public function __construct(){
     parent::__construct(
         'laboratorium',
         'ref_status_permintaan',
         [
-            'id_status'   => T::ID8(),
+            'id_status'   => T::ID8(10),
             'nama_status' => T::TEXT(),
         ],
         'id_status',
         [],
         [],
         true,
-        __DIR__ . '/status_permintaan.csv'
+        'status_permintaan.csv'
     );
 }
 }
