88aeee0a83190c51e95f6732f4a0e20a7481f3a1
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-05-08 10:43:31 +0700

    Add more expressive type in ValidationType

--- app/Core/Model/ValidationType.php
+++ app/Core/Model/ValidationType.php
@@ -17,4 +17,53 @@ final class ValidationType
     public static function DEFAULT(): self {
         return new self('DEFAULT', 'DEFAULT');
     }
+
+    public static function INDEX(): self {
+        return new self('SELF', 'SELF');
+    }
+
+    public static function FLOAT(): self {
+        return new self('FLOAT', 'desimal');
+    }
+
+    public static function MONEY(): self {
+        return new self('MONEY', 'uang');
+    }
+
+    public static function DTIME(): self {
+        return new self('DTIME', 'tanggal_jam');
+    }
+
+    public static function DATE(): self {
+        return new self('DATE', 'tanggal');
+    }
+
+    public static function TIME(): self {
+        return new self('TIME', 'jam');
+    }
+
+    public static function NAME(): self {
+        return new self('NAME', 'nama');
+    }
+
+    public static function TEXT(): self {
+        return new self('TEXT', 'teks');
+    }
+
+    public static function BOOL(): self {
+        return new self('BOOL', 'bool');
+    }
+
+    public static function TEMP(): self {
+        return new self('TEMP', 'suhu');
+    }
+
+    public static function NUMBER(): self {
+        return new self('NUMBER', 'jumlah');
+    }
+
+    public static function SELECT(): self {
+        return new self('SELECT', 'status');
+    }
+    
 }
