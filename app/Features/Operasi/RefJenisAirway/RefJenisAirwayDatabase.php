bcf3d6d543d106cd204982534254934d242f6768
qoyyimil <qoyyimillj@gmail.com>
2026-05-11 11:50:17 +0700

    Add Feature RefJenisAirway

---
+++ app/Features/Operasi/RefJenisAirway/RefJenisAirwayDatabase.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\RefJenisAirway;
+
+use App\Core\Database\DatabaseTemplate;
+use App\Core\Database\Template\SemanticType as T;
+    
+final class RefJenisAirwayDatabase extends DatabaseTemplate
+{
+    public function __construct(){
+    parent::__construct(
+        'operasi',
+        'ref_jenis_airway',
+        [
+            'id_jenis'    => T::ID8(10),
+            'nama_jenis'  => T::TEXT(),
+        ],
+        'id_jenis',
+        [],
+        [],
+        true,
+        'jenis_airway.csv'
+    );
+}
+}
