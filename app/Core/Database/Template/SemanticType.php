f4c39abf2cb32d74b2bffa6c88ae4d9436878e69
Aliyah <aliyahawwaliyah@gmail.com>
2026-05-12 14:36:21 +0700

    change RECORD type from TEXT to VARCHAR

--- app/Core/Database/Template/SemanticType.php
+++ app/Core/Database/Template/SemanticType.php
@@ -9,7 +9,7 @@ final readonly class SemanticType
 {
     public static function FK_AUTO(): FT { return new FT('FK_AUTO');}
     public static function ID(int $max): FT { return PT::ID($max); }
-    public static function RECORD(): FT { return PT::TEXT();}
+    public static function RECORD(int $len): FT { return PT::VARCHAR($len);}
     public static function BOOL(): FT { return PT::BOOL();}
 
     public static function QTY(int $min, int $max): FT { return PT::INT($min, $max);}
