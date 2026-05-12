f9265a3dba39ef76a9aab2fb7f1c845747bf3e37
qoyyimil <qoyyimillj@gmail.com>
2026-05-12 13:13:13 +0700

    Fix PermintaanLabPa database and model

--- app/Features/Laboratorium/PermintaanLabPa/PermintaanLabPaModel.php
+++ app/Features/Laboratorium/PermintaanLabPa/PermintaanLabPaModel.php
@@ -10,23 +10,28 @@ final class PermintaanLabPaModel extends ModelTemplate
     public function __construct()
     {
         parent::__construct(
+            new PermintaanLabPaDatabase(),
             'BASE',
             'laboratorium',
             'permintaan_lab_pa',
             'id_permintaan_pa',
             [
-                'id_permintaan_pa' => V::TODO(),
-                'id_permintaan_lab' => V::TODO(),
-                'tgl_pengambilan_bahan' => V::TODO(),
-                'metode_diperoleh' => V::TODO(),
-                'lokasi_jaringan' => V::TODO(),
-                'bahan_pengawet' => V::TODO(),
-                'riwayat_lokasi_lab' => V::TODO(),
-                'riwayat_tgl_sebelumnya' => V::TODO(),
-                'riwayat_no_pa_sebelumnya' => V::TODO(),
-                'riwayat_diagnosa_sebelumnya' => V::TODO(),
-                'id_item_pemeriksaan' => V::TODO(),
+                'id_permintaan_pa'              => V::DEFAULT(),
+                'id_permintaan_lab'             => V::DEFAULT(),
+                'tgl_pengambilan_bahan'         => V::DEFAULT(),
+                'metode_diperoleh'              => V::DEFAULT(),
+                'lokasi_jaringan'               => V::DEFAULT(),
+                'bahan_pengawet'                => V::DEFAULT(),
+                'riwayat_lokasi_lab'            => V::DEFAULT(),
+                'riwayat_tgl_sebelumnya'        => V::DEFAULT(),
+                'riwayat_no_pa_sebelumnya'      => V::DEFAULT(),
+                'riwayat_diagnosa_sebelumnya'   => V::DEFAULT(),
+                'id_item_pemeriksaan'           => V::DEFAULT(),
             ],
+            [
+                'id_permintaan_lab'   => [],
+                'id_item_pemeriksaan' => ['kode_periksa', 'nama_item', 'tarif'],
+           ]
         );
     }
 }
