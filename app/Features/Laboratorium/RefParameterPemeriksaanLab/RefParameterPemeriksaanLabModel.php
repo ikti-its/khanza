f0353ca3c32cbd30690f09a166fe95a03bea1af9
qoyyimil <qoyyimillj@gmail.com>
2026-05-13 13:48:15 +0700

    Fix RefParameterPemeriksaanLab database and model

--- app/Features/Laboratorium/RefParameterPemeriksaanLab/RefParameterPemeriksaanLabModel.php
+++ app/Features/Laboratorium/RefParameterPemeriksaanLab/RefParameterPemeriksaanLabModel.php
@@ -10,18 +10,22 @@ final class RefParameterPemeriksaanLabModel extends ModelTemplate
     public function __construct()
     {
         parent::__construct(
+            new RefParameterPemeriksaanLabDatabase(),
             'REFS',
             'laboratorium',
             'ref_parameter_pemeriksaan_lab',
             'id_parameter',
             [
-                'id_parameter' => V::TODO(),
-                'id_item_lab' => V::TODO(),
-                'nama_parameter' => V::TODO(),
-                'satuan' => V::TODO(),
-                'nilai_rujukan' => V::TODO(),
-                'biaya_item' => V::TODO(),
+                'id_parameter'    => V::DEFAULT(),
+                'id_item_lab'     => V::DEFAULT(),
+                'nama_parameter'  => V::DEFAULT(),
+                'satuan'          => V::DEFAULT(),
+                'nilai_rujukan'   => V::DEFAULT(),
+                'biaya_item'      => V::DEFAULT(),
             ],
+            [
+                'id_item_lab'   => ['kode_periksa', 'nama_item', 'tarif'],
+            ]
         );
     }
 }
