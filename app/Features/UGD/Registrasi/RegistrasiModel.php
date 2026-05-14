339e6b8945394fbbf8be454829d641af27024d3e
Aliyah <aliyahawwaliyah@gmail.com>
2026-05-14 11:19:22 +0700

    Add feature Registrasi in UGD

---
+++ app/Features/UGD/Registrasi/RegistrasiModel.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UGD\Registrasi;
+use App\Core\Model\ModelTemplate;
+use App\Core\Model\ValidationType as V;
+
+final class RegistrasiModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            new RegistrasiDatabase(),
+            'BASE',
+            'ugd',
+            'registrasi',
+            'nomor_reg',
+            [
+                'nomor_reg'         => V::DEFAULT(),
+                'nomor_rawat'       => V::DEFAULT(),
+                'tanggal'           => V::DEFAULT(),
+                'jam'               => V::DEFAULT(),
+                'kode_dokter'       => V::DEFAULT(),
+                'dokter_dituju'     => V::DEFAULT(),
+                'nomor_rm'          => V::DEFAULT(),
+                'nama_pasien'       => V::DEFAULT(),
+                'jenis_kelamin'     => V::DEFAULT(),
+                'poliklinik'        => V::DEFAULT(),
+                'penanggung_jawab'  => V::DEFAULT(),
+                'alamat_pj'         => V::DEFAULT(),
+                'hubungan_pj'       => V::DEFAULT(),
+                'biaya_registrasi'  => V::DEFAULT(),
+                'jenis_bayar'       => V::DEFAULT(),
+                'status_rawat'      => V::DEFAULT(),
+                'status_bayar'      => V::DEFAULT()
+            ],
+            [],
+        );
+    }
+}
