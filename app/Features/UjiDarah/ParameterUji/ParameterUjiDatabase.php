9aad894932e60903c7b5917f002ab1cdf4c3d069
Aliyah <aliyahawwaliyah@gmail.com>
2026-05-16 13:26:52 +0700

    Add feature Parameter Uji in Uji Darah

---
+++ app/Features/UjiDarah/ParameterUji/ParameterUjiDatabase.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UjiDarah\ParameterUji;
+
+use App\Core\Database\DatabaseTemplate;
+use App\Core\Database\Template\SemanticType as T;
+
+final class ParameterUjiDatabase extends DatabaseTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            'uji_darah',
+            'parameter_uji',
+            [
+                'id_parameter_uji' => T::ID(10),
+                'nama_parameter'   => T::NAME(20),
+            ],
+            'id_parameter_uji',
+            ['nama_parameter'],
+            [],
+            true,
+            'parameter_uji.csv'
+        );
+    }
+}
---
+++ app/Features/UjiDarah/ParameterUji/ParameterUjiModel.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UjiDarah\ParameterUji;
+use App\Core\Model\ModelTemplate;
+use App\Core\Model\ValidationType as V;
+
+final class ParameterUjiModel extends ModelTemplate
+{
+    public function __construct(){
+        parent::__construct(
+            new ParameterUjiDatabase(),
+            'REFS',
+            'uji_darah',
+            'parameter_uji',
+            'id_parameter_uji',
+            [
+                'id_parameter_uji'  => V::DEFAULT(),
+                'nama_parameter'    => V::DEFAULT()
+            ],
+            [],
+        );
+    }
+}
