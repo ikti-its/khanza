42f7bad76737d0a3d3738435224b4e3deffaa8ac
Aliyah <aliyahawwaliyah@gmail.com>
2026-05-21 18:51:54 +0700

    Add conditional visibility for Pisahkan button based on status

--- app/Views/components/aksi/pemisahan_komponen.php
+++ app/Views/components/aksi/pemisahan_komponen.php
@@ -1,6 +1,14 @@
-<div class="px-3 py-1.5">
-    <a href="<?= base_url('inventaris-darah/pemisahan-komponen/tambah?pengambilan=' . $id) ?>" 
-       class="gap-x-1 text-sm text-teal-600 hover:underline font-semibold dark:text-teal-400">
-        Pisahkan
-    </a>
+<div class="w-full">
+    <?php if (isset($baris['id_status_pengambilan']) && (int)$baris['id_status_pengambilan'] === 1): ?>
+        <div class="px-3 py-1.5">
+            <a href="<?= base_url('inventaris-darah/pemisahan-komponen/tambah?pengambilan=' . $id) ?>" 
+               class="gap-x-1 text-sm text-teal-600 hover:underline font-semibold dark:text-teal-400">
+                Pisahkan
+            </a>
+        </div>
+    <?php else: ?>
+        <div class="px-3 py-1.5">
+            <a class="text-sm text-teal-600 hover:underline font-semibold" style="visibility:hidden">Pisahkan</a>
+        </div>
+    <?php endif; ?>
 </div>
