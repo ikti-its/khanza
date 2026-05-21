b4e82decee9376b8afc417af17ae30c025713cdd
qoyyimil <qoyyimillj@gmail.com>
2026-05-21 15:03:44 +0700

    Refactor joining tables using the new general hospital room master

--- app/Features/Operasi/JadwalOperasi/JadwalOperasiDatabase.php
+++ app/Features/Operasi/JadwalOperasi/JadwalOperasiDatabase.php
@@ -33,7 +33,7 @@ final class JadwalOperasiDatabase extends DatabaseTemplate
             ],
             [
                 ['id_ruangan'],
-                \App\Features\Operasi\RefRuanganOperasi\RefRuanganOperasiDatabase::class,
+                \App\Features\Ruangan\RuanganDatabase::class,
                 ['id_ruangan'],
             ],
             // ['id_tindakan', 'sik.jenis_tindakan_structure', 'kode'],
