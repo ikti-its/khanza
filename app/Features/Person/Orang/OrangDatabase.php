7f68fbed0d2a9d9cdda3a365af9a5d5380c90465
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-05-24 15:56:01 +0700

    Add declare(strict_types=1); in Person

--- app/Features/Person/Orang/OrangDatabase.php
+++ app/Features/Person/Orang/OrangDatabase.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Features\Person\Orang;
 use App\Core\Database\Template\DatabaseTemplate;
--- app/Features/Person/Pernikahan/PernikahanDatabase.php
+++ app/Features/Person/Pernikahan/PernikahanDatabase.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Features\Person\Pernikahan;
 use App\Core\Database\Template\DatabaseTemplate;
