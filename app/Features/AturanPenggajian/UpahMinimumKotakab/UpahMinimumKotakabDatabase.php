8ba1192e7da178234343aed7b61f53f1a7c46223
qoyyimil <qoyyimillj@gmail.com>
2026-05-31 23:09:03 +0700

    Rename umk seed

--- app/Features/AturanPenggajian/UpahMinimumKotakab/UpahMinimumKotakabDatabase.php
+++ app/Features/AturanPenggajian/UpahMinimumKotakab/UpahMinimumKotakabDatabase.php
@@ -31,7 +31,7 @@ final class UpahMinimumKotakabDatabase extends DatabaseTemplate
                 ],
             ],
             true,
-            'upmk.csv',
+            'umk.csv',
         );
     }
 }
